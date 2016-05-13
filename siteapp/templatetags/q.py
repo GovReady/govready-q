d4d1695282072381650f2a95135b0c954f577de7
Joshua Tauberer <jt@occams.info>
2016-05-13 12:15:48 -0400

    forgot to commit this at some point

---
+++ siteapp/templatetags/q.py
@@ -0,0 +1,16 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter(is_safe=True)
+@stringfilter
+def markdown(value):
+    import CommonMark
+    return mark_safe(CommonMark.commonmark(str(value), safe_mode=True))
+
+@register.filter(is_safe=True)
+def json(value):
+    import json
+    return mark_safe(json.dumps(value))
