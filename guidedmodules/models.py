GovReady/govready-q

commit 751f956887c0643eb408d5304ffcec3b44962932

deleting tasks, fixes #17
If a user has read privs on a task, then they see a message that the "module was deleted" when they try to go to it. Except discussion guests.

Also fixes a logic bug in testing if invitations have expired.

guidedmodules/models.py
@@ -17,6 +17,8 @@ class Task(models.Model):
 
     created = models.DateTimeField(auto_now_add=True, db_index=True)
     updated = models.DateTimeField(auto_now=True, db_index=True)
+    deleted_at = models.DateTimeField(blank=True, null=True, db_index=True, help_text="If 'deleted' by a user, the date & time the Task was deleted.")
+
     extra = JSONField(blank=True, help_text="Additional information stored with this object.")
 
     invitation_history = models.ManyToManyField('siteapp.Invitation', blank=True, help_text="The history of accepted invitations that had this Task as a target.")
@@ -35,19 +37,27 @@ class Task(models.Model):
         return "<Task [%d] %s %s>" % (self.id, self.title[0:30], repr(self.project))
 
     @staticmethod
-    def has_completed_task(user, module_id, project=None):
-        task = Task.objects.filter(project=project, editor=user, module_id=module_id).first()
-        if task is None:
-            return False
-        return task.is_finished()
+    def has_completed_task(user, module_id):
+        task = Task.get_task_for_module(user, module_id, create=False)
+        return task and task.is_finished()
 
     @staticmethod
-    def get_task_for_module(user, module_id):
+    def get_task_for_module(user, module_id, create=True):
         # Gets a task given a module_id. Use only with system modules
         # where a user can only have one Task for the module.
-        task, isnew = Task.objects.get_or_create(
-            editor=user,
-            module_id=module_id)
+
+        filters = {
+            "editor": user,
+            "module_id": module_id,
+            "project": None,
+            "deleted_at": None,
+        }
+
+        if not create:
+            # Check for existence first. If none exists, return None.
+            return Task.objects.filter(**filters).first()
+
+        task, isnew = Task.objects.get_or_create(**filters)
         if isnew:
             task.title = task.load_module().title
             task.save()
@@ -104,17 +114,24 @@ class Task(models.Model):
     @staticmethod
     def get_all_tasks_readable_by(user):
         # symmetric with has_read_priv
-        return (Task.objects.filter(editor=user)
-               | Task.objects.filter(project__members__user=user)).distinct()
+        return Task.objects.filter(
+            models.Q(editor=user) | models.Q(project__members__user=user),
+            deleted_at=None,
+            )
 
-    def has_read_priv(self, user):
-        if self.has_write_priv(user):
+    def has_read_priv(self, user, allow_access_to_deleted=False):
+        # symmetric get_all_tasks_readable_by has_read_priv
+        if self.deleted_at and not allow_access_to_deleted:
+            return False
+        if self.has_write_priv(user, allow_access_to_deleted=allow_access_to_deleted):
             return True
         if ProjectMembership.objects.filter(project=self.project, user=user).exists():
             return True
         return False
 
-    def has_write_priv(self, user):
+    def has_write_priv(self, user, allow_access_to_deleted=False):
+        if self.deleted_at and not allow_access_to_deleted:
+            return False
         if self.editor == user:
             # The editor.
             return True
@@ -132,9 +149,10 @@ class Task(models.Model):
                 + (" and to join the project team" if invitation.into_project else "")
 
     def is_invitation_valid(self, invitation):
-        # Invitation remains valid only if the user that sent it is still
-        # the editor.
-        return self.editor == invitation.from_user
+        # Invitation remains valid only if the user that sent it still
+        # has write privs to the Task (i.e. task is not deleted, user
+        # is the editor or an admin)
+        return self.has_write_priv(invitation.from_user)
 
     def accept_invitation(self, invitation, add_message):
         # Make this user the new editor.

discussion/models.py
@@ -44,6 +44,14 @@ class Discussion(models.Model):
         return self.attached_to.task.title + ": " + self.attached_to.get_question().title
 
     def is_participant(self, user):
+        # No on is a participant of a dicussion attached to (a question
+        # of) a deleted Task.
+        if self.attached_to.task.deleted_at:
+            return False
+
+        # Participants are members of the project team of the task of
+        # the question this Discussion is attached to, plus the Discussion's
+        # external_participants.
         return (ProjectMembership.objects.filter(project=self.attached_to.project, user=user)) \
             or (user in self.external_participants.all())
 

siteapp/models.py
@@ -212,7 +212,7 @@ class Invitation(models.Model):
             return True
 
         if not self.target.is_invitation_valid(self):
-            return False
+            return True
 
         return False
     is_expired.boolean = True

guidedmodules/migrations/0003_task_deleted_at.py
@@ -1,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Generated by Django 1.9 on 2016-05-20 13:09
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('guidedmodules', '0002_auto_20160518_2206'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='task',
+            name='deleted_at',
+            field=models.DateTimeField(blank=True, db_index=True, help_text="If 'deleted' by a user, the date & time the Task was deleted.", null=True),
+        ),
+    ]

guidedmodules/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'(\d+)/([\w_-]+)(/start)?$', guidedmodules.views.next_question),
     url(r'start$', guidedmodules.views.new_task),
     url(r'new-project$', guidedmodules.views.new_project),
+    url(r'_change_state$', guidedmodules.views.change_task_state, name="task_change_state"),
 ]
 

guidedmodules/views.py
@@ -182,18 +182,28 @@ def next_question(request, taskid, taskslug, intropage=None):
 
     # Does the user have read privs here?
     def read_priv():
-        if task.has_read_priv(request.user):
+        if task.has_read_priv(request.user, allow_access_to_deleted=True):
+            # See below for checking if the task was deleted.
             return True
         if not taskq:
             return False
         d = Discussion.get_for(taskq)
         if not d:
             return False
-        if request.user in d.external_participants.all():
+        if d.is_participant(request.user):
             return True
         return False
     if not read_priv():
         return HttpResponseForbidden()
+
+    # We skiped the check for whether the Task is deleted above. Now
+    # check for that.
+    if task.deleted_at:
+        # The Task is deleted. If the user would have had access to it,
+        # show a more friendly page than an access denied. Discussion
+        # guests will have been denied above because is_participant
+        # will fail on deleted tasks.
+        return HttpResponse("This module was deleted by its editor or a project administrator.")
 
     # Redirect if slug is not canonical. We do this after checking for
     # read privs so that we don't reveal the task's slug to unpriv'd users.
@@ -245,3 +255,23 @@ def next_question(request, taskid, taskslug, intropage=None):
             "answer_answered_by_task_can_write": answer.answered_by_task.has_write_priv(request.user) if answer and answer.answered_by_task else None,
         })
         return render(request, "question.html", context)
+
+@login_required
+def change_task_state(request):
+    if request.method != "POST":
+        return HttpResponseNotAllowed(["POST"])
+
+    task = get_object_or_404(Task, id=request.POST["id"])
+    if not task.has_write_priv(request.user, allow_access_to_deleted=True):
+        return HttpResponseForbidden()
+
+    if request.POST['state'] == "delete":
+        task.deleted_at = timezone.now()
+    elif request.POST['state'] == "undelete":
+        task.deleted_at = None
+    else:
+        return HttpResponseForbidden()
+
+    task.save(update_fields=["deleted_at"])
+
+    return HttpResponse("ok")

siteapp/views.py
@@ -108,7 +108,9 @@ def homepage(request):
                 "tasks": [],
                 "others_tasks": [],
                 "discussions": [],
-                "open_invitations": Invitation.objects.filter(from_user=request.user, from_project=project, accepted_at=None, revoked_at=None).order_by('-created')
+                "open_invitations": [
+                    inv for inv in Invitation.objects.filter(from_user=request.user, from_project=project, accepted_at=None, revoked_at=None).order_by('-created')
+                    if not inv.is_expired() ]
                     if project else None,
                 "startable_modules": Module.get_anserable_modules()
                     if project else None,
@@ -129,6 +131,10 @@ def homepage(request):
                 "tasks" if task.editor == request.user else "others_tasks"
             ].append(task)
             seen_tasks.add(task)
+
+            # Annotate task object with whether the user has write priv
+            # on it - because the user can delete these.
+            task.user_has_write_priv = task.has_write_priv(request.user)
 
         # Including tasks the user is participating in a discussion about
         # (but would not otherwise have read permission).

templates/base.html
@@ -46,6 +46,8 @@
 
         <link rel="stylesheet" href="{% static "css/main.css" %}">
 
+        {% block head %}{% endblock %}
+
         <!--[if lt IE 9]>
             <script src="static/js/html5-3.6-respond-1.1.0.min.js"></script>
         <![endif]-->

commit f53c74761a0bc7a28332f55c7bc1a7993d1071a7

get_all_tasks_readable_by was returning duplicates, broken by 751f9568

guidedmodules/models.py
@@ -166,7 +166,7 @@ class Task(models.Model):
         return Task.objects.filter(
             models.Q(editor=user) | models.Q(project__members__user=user),
             deleted_at=None,
-            )
+            ).distinct()
 
     def has_read_priv(self, user, allow_access_to_deleted=False):
         # symmetric get_all_tasks_readable_by has_read_priv