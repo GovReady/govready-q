GovReady/govready-q

commit 41cca895ba0b3580ae085e6a2d392424ced8cb47

allow non-admin project members to start tasks on the project page

siteapp/models.py
@@ -89,8 +89,14 @@ class Project(models.Model):
         from django.utils.text import slugify
         return "/projects/%d/%s" % (self.id, slugify(self.title))
 
+    def get_members(self):
+        return User.objects.filter(projectmembership__project=self)
+
     def get_admins(self):
-        return User.objects.filter(projectmembership__project=self, projectmembership__is_admin=True)
+        return self.get_members().filter(projectmembership__is_admin=True)
+
+    def can_start_task(self, user):
+        return (not self.is_account_project) and (user in self.get_members())
 
     def can_invite_others(self, user):
         return (not self.is_account_project) and (user in self.get_admins())

guidedmodules/views.py
@@ -16,7 +16,7 @@ def new_task(request):
     project = get_object_or_404(Project, id=request.POST["project"])
 
     # Can the user create a task within this project?
-    if request.user not in project.get_admins():
+    if not project.can_start_task(request.user):
         return HttpResponseForbidden()
 
     # Create the new subtask.

siteapp/views.py
@@ -213,7 +213,7 @@ def project(request, project_id):
     return render(request, "project.html", {
         "is_admin": request.user in project.get_admins(),
         "is_member": is_project_member,
-        "can_begin_module": (request.user in project.get_admins()) and not project.is_account_project,
+        "can_begin_module": project.can_start_task(request.user),
         "project_has_members_besides_me": project and project.members.exclude(user=request.user),
         "project": project,
         "title": project.title,

commit 8699fcca001691ce231cf54f8e9e079bfdbce051

project admin list broken by 41cca895ba0b3580ae085e6a2d392424ced8cb47
Must keep filters on the same table join together.

Fixes #82, which resulted from an incorrect admins check.

siteapp/models.py
@@ -93,7 +93,7 @@ class Project(models.Model):
         return User.objects.filter(projectmembership__project=self)
 
     def get_admins(self):
-        return self.get_members().filter(projectmembership__is_admin=True)
+        return User.objects.filter(projectmembership__project=self, projectmembership__is_admin=True)
 
     def can_start_task(self, user):
         return (not self.is_account_project) and (user in self.get_members())