d737e291abb6842ffad1c9422b44a7f5565568fc
Joshua Tauberer <jt@occams.info>
2016-06-24 15:28:10 -0400

    demo2_analyze_site now uses the GovReady CMS API
    The environment.json file now needs:
    
        "govready_cms_api_auth": [username, password]

--- guidedmodules/sample_external_functions.py
+++ guidedmodules/sample_external_functions.py
@@ -1,21 +1,39 @@
+from django.conf import settings
+
 from datetime import datetime
 
 def sample_function(question, existing_answers):
-	url = existing_answers.get("project_url")
-	if not url:
-		raise ValueError("The website URL is not set.")
+    import urllib.request, urllib.parse
+    import base64, json
 
-	try:
-		import urllib.request
-		resp = urllib.request.urlopen(url)
-		headers = resp.info()
-	except Exception as e:
-		raise ValueError(str(e))
+    project_url = existing_answers.get("project_url")
+    if not project_url:
+        raise ValueError("The website URL is not set.")
 
-	return {
-		"scan_date": datetime.now().isoformat(),
-		"url": url,
-		
-		"server": headers['Server'],
-		"hsts": 'Strict-Transport-Security' in headers,
-	}
+    # Get authentication credentials for the GovReady CMS API.
+    auth_user, auth_pw = settings.GOVREADY_CMS_API_AUTH
+
+    # Ask the GovReady CMS API for informationa about the website.
+    try:
+        url = "https://plugin.govready.com/v1.0/public/info?" + urllib.parse.urlencode({
+            "url": project_url
+        })
+        req = urllib.request.Request(url)
+        req.add_header('authorization', b'Basic ' + base64.b64encode(auth_user.encode("ascii") + b":" + auth_pw.encode("ascii")))
+        req.add_header('cache-control', 'no-cache')
+        resp = json.loads(urllib.request.urlopen(req).read().decode("utf8"))
+    except Exception as e:
+        raise ValueError("There was an error running the analysis: " + str(e))
+
+    return {
+        "scan_date": datetime.now().isoformat(),
+        "url": url,
+        "result": resp
+    }
+
+if __name__ == "__main__":
+    import sys, os, rtyaml
+    sys.path.insert(0, '.')
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'siteapp.settings'
+    ret = sample_function(None, { "project_url": sys.argv[1] })
+    print(rtyaml.dump(ret))
--- siteapp/settings_application.py
+++ siteapp/settings_application.py
@@ -16,3 +16,5 @@ AUTHENTICATION_BACKENDS += ['siteapp.models.DirectLoginBackend']
 
 SERVER_EMAIL = "GovReady Q <q@mg.govready.com>"
 DEFAULT_FROM_EMAIL = SERVER_EMAIL
+
+GOVREADY_CMS_API_AUTH = environment['govready_cms_api_auth']
