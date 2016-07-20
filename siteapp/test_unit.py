3581a707b053d0ce255e98331f9035ee7aa78427
Peter Burkholder <pburkholder@pobox.com>
2016-07-20 17:35:06 -0400

    Both tests pass again

--- siteapp/test_unit.py
+++ siteapp/test_unit.py
@@ -3,13 +3,25 @@
 
 from test.support import EnvironmentVarGuard
 import unittest
+from django.conf import settings
+settings.configure()
 
 class TestEnv(unittest.TestCase):
+    def setUp(self):
+        self.env = EnvironmentVarGuard()
+        self.env.set('SECRET_KEY', 'xoxo')
+        self.env.set(
+            'DATABASE_URL',
+             'bogus://myuser:mypass@myhost3:3306/my_database_name'
+             )
+
     def test_secret_key(self):
-        with EnvironmentVarGuard() as environ:
-            environ['SECRET_KEY'] = 'xoxo'
-            from django.conf import settings
-            settings.configure()
-            from siteapp import settings
-            
-            self.assertEqual(settings.SECRET_KEY,'xoxo')
+        from siteapp import settings
+        self.assertEqual(settings.SECRET_KEY,'xoxo')
+
+    def test_db_url(self):
+        from siteapp import settings
+        self.assertEqual(
+            settings.DATABASES['default']['HOST'],
+            'myhost3'
+            )
