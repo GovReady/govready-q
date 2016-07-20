89abf023f6ccfc4a9749dcc18d903297e524ad8e
Peter Burkholder <pburkholder@pobox.com>
2016-07-20 16:54:15 -0400

    Only the settings, please

--- siteapp/settings.py
+++ siteapp/settings.py
@@ -1,14 +1,19 @@
 # from https://raw.githubusercontent.com/if-then-fund/django-good-settings/master/settings.py
 
 ################################################################
-# Good defaults for a setttings.py, plus logic for bringing    #
-# in settings from various normal places you might store them. #
+# Good defaults for a setttings.py:
+#   - reads environment/local.json, if available
+#   - sets values in order of:
+#     1. env value
+#     2. environment/local.json value
+#     3. default value
 ################################################################
 
 import os, os.path, json
 
 # What's the name of the app containing this file? That determines
-# the module for the main URLconf etc.
+# the module for the main URLconf etc. E.g. `govready-q/siteapp/settings.py`
+# resolves to 'siteapp'
 primary_app = os.path.basename(os.path.dirname(__file__))
 
 # LOAD ENVIRONMENT SETTINGS #
@@ -22,7 +27,7 @@ def local(fn):
 if os.path.exists(local("environment.json")):
 	environment = json.load(open(local("environment.json")))
 else:
-	# Make some defaults.
+	# Make some defaults for local development
 
 	# This is how 'manage.py startproject' does it:
 	def make_secret_key():
@@ -36,22 +41,71 @@ else:
 		"https": False,
 	}
 
+#    if  os.getenv('SECRET_KEY') == None:
 	print("Create a local/environment.json file! It should contain something like this:")
 	print(json.dumps(environment, sort_keys=True, indent=2))
-	
+
 # DJANGO SETTINGS #
 ###################
 
+# Check system environment vars, then the local/environment.json settings
+
+# Required settings
 # The SECRET_KEY must be specified in the environment.
-SECRET_KEY = environment["secret-key"]
-
-# The DEBUG flag must be set in the environment.
-DEBUG = environment["debug"]
-ADMINS = environment.get("admins", [])
-
+SECRET_KEY    = os.getenv('SECRET_KEY', environment["secret-key"])
+DEBUG         = os.getenv('DEBUG', environment['debug'])
 # Set ALLOWED_HOSTS from the host environment. If it has a port, strip it.
 # The port is used in SITE_ROOT_URL must must be removed from ALLOWED_HOSTS.
-ALLOWED_HOSTS = [environment["host"].split(':')[0]]
+HOST          = os.getenv('HOST', environment['host'])
+#ALLOWED_HOSTS = HOST.split(':')[0]
+ALLOWED_HOSTS = "localhost:800".split(':')[0]
+HTTPS         = os.getenv('HTTPS',environment["https"])
+
+## Optional settings either from env variables or local/environment.json
+ADMINS        = os.getenv('ADMINS', environment.get("admins", []))
+USE_MEMCACHED = os.getenv('USE_MEMCACHED', environment.get('memcached'))
+EMAIL         = os.getenv('EMAIL', environment.get('email'))
+# STATIC True (as for CF deploys) uses a relative path, STATIC "path"
+# uses that path:
+STATIC        = os.getenv('STATIC', environment.get('static'))
+
+################ DATABASE SETUP ##############
+# Use an Sqlite database at local/db.sqlite, until other database
+# settings have been set in the environment.
+#
+# when DATABASE_URL='mysql2://myuser:mypass@myhost:3306/my_database_name'
+# then DATABASES['default'] =
+#    {'USER': 'myuser', 'NAME': 'my_database_name', 'HOST': 'myhost', 'PASSWORD': 'mypass', 'PORT': 3306, 'ENGINE': 'django.db.backends.mysql'}
+# else DATABASES['default'] =
+#    {'ENGINE': 'django.db.backends.sqlite3', 'PASSWORD': None, 'USER': None, 'NAME': 'local/db.sqlite3', 'PORT': None, 'HOST': None}
+
+from urllib.parse import urlparse
+url = urlparse(
+		os.environ.get(
+            'DATABASE_URL',
+            'sqlite3:///db.sqlite3'
+			)
+        )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql' if 'mysql' in url.scheme else 'django.db.backends.sqlite3',
+        'NAME': url.path[1:] if url.scheme == 'mysql2' else local(url.path[1:]),
+	    'CONN_MAX_AGE': 60 if url.scheme != 'sqlite3' else None,
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
+    }
+}
+
+# print("Using DB: %s", (DATABASES['default']))
+# Ensure the 'local' directory exists for the default Sqlite
+# database.
+if 'sqlite3' in DATABASES['default']['NAME']:
+	if not os.path.exists(os.path.dirname(local('.'))):
+		print("Making local dir for SQLite3 DB")
+		os.mkdir(os.path.dirname(local('.')))
+################ FINI DATABASE SETUP ##############
 
 # allauth requires the use of the sites framework.
 SITE_ID = 1
@@ -92,7 +146,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 ]
-if environment["debug"] and os.path.exists(os.path.join(os.path.dirname(__file__), 'helper_middleware.py')):
+if DEBUG and os.path.exists(os.path.join(os.path.dirname(__file__), 'helper_middleware.py')):
 	MIDDLEWARE_CLASSES.append(primary_app+'.helper_middleware.DumpErrorsToConsole')
 
 # Load templates for app directories and from a main `templates` directory located
@@ -143,7 +197,7 @@ ACCOUNT_ADAPTER = primary_app + '.good_settings_helpers.AllauthAccountAdapter'
 ACCOUNT_AUTHENTICATION_METHOD = 'username'
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True
 ACCOUNT_UNIQUE_EMAIL = False # otherwise unconfirmed addresses may block real users
-ACCOUNT_DEFAULT_HTTP_PROTOCOL = ("http" if not environment["https"] else "https")
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = ("http" if not HTTPS else "https")
 ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 15 # default of 5 is too low!
 ACCOUNT_LOGOUT_ON_GET = True # allow simplified logout link
 ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
@@ -154,24 +208,6 @@ ACCOUNT_PASSWORD_MIN_LENGTH = (4 if DEBUG else 6) # in debugging, allow simple p
 from bootstrapform.templatetags.bootstrap import bootstrap#_horizontal
 ALLAUTH_FORM_RENDERER = bootstrap#_horizontal
 
-# Use an Sqlite database at local/db.sqlite, until other database
-# settings have been set in the environment.
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': local('db.sqlite3'),
-	}
-}
-if not environment.get('db'):
-	# Ensure the 'local' directory exists for the default Sqlite
-	# database.
-	if not os.path.exists(os.path.dirname(local('.'))):
-		os.mkdir(os.path.dirname(local('.')))
-else:
-	# Enable database connection pooling (unless overridden in the
-	# environment settings).
-	DATABASES['default']['CONN_MAX_AGE'] = 60
-	DATABASES['default'].update(environment['db'])
 
 # Setup the cache. The default is a LocMemCache.
 CACHES = {
@@ -180,7 +216,7 @@ CACHES = {
 		'LOCATION': '127.0.0.1:11211',
 	}
 }
-if environment.get('memcached'):
+if USE_MEMCACHED:
 	# But if the 'memcached' environment setting is true,
 	# enable a memcached cache using the default host/port
 	# (see above) *and* enable the cached_db session backend.
@@ -210,36 +246,41 @@ USE_L10N = True
 # providing an SMTP server to send outbound emails to. TLS is
 # always turned on.
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_SUBJECT_PREFIX = '[' + environment['host'] + '] '
-if environment.get("email"):
+EMAIL_SUBJECT_PREFIX = '[' + HOST + '] '
+if EMAIL:
 	EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-	EMAIL_HOST = environment["email"]["host"]
-	EMAIL_PORT = environment["email"]["port"]
-	EMAIL_HOST_USER = environment["email"]["user"]
-	EMAIL_HOST_PASSWORD = environment["email"]["pw"]
+	EMAIL_HOST = EMAIL["host"]
+	EMAIL_PORT = EMAIL["port"]
+	EMAIL_HOST_USER = EMAIL["user"]
+	EMAIL_HOST_PASSWORD = EMAIL["pw"]
 	EMAIL_USE_TLS = True
 
 # If the "https" environment setting is true, set the settings
 # that keep sessions and cookies secure.
-if environment["https"]:
+if HTTPS:
 	SESSION_COOKIE_HTTPONLY = True
 	SESSION_COOKIE_SECURE = True
 	CSRF_COOKIE_HTTPONLY = True
 	CSRF_COOKIE_SECURE = True
 
 # Put static files in the virtual path "/static/". When the "static"
-# environment setting is present, then it's a local directory path
-# where "collectstatic" will put static files. The ManifestStaticFilesStorage
-# is activated.
+# environment setting is set to a string, then it's a local directory path
+# where "collectstatic" will put static files. If "static" is simply True, use a
+# relative path.
+# The ManifestStaticFilesStorage is activated.
 STATIC_URL = '/static/'
-if environment.get("static"):
-	STATIC_ROOT = environment["static"]
+if STATIC in ['True', 'true', True]:
+	PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+	STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+	STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+elif STATIC:
+	STATIC_ROOT = STATIC
 	STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Add a convenience setting "SITE_ROOT_URL" that stores the root URL
 # of the website, constructed from the "https" and "host" environment
 # settings
-SITE_ROOT_URL = "%s://%s" % (("http" if not environment["https"] else "https"), environment["host"])
+SITE_ROOT_URL = "%s://%s" % (("http" if not HTTPS else "https"), HOST)
 
 # Load all additional settings from settings_application.py.
 from .settings_application import *
