c2bd657446a1d8801e1db33cebd9f19fca51dfa9
Peter Burkholder <pburkholder@pobox.com>
2016-07-20 17:17:00 -0400

    Env test works

--- siteapp/test_unit.py
+++ siteapp/test_unit.py
@@ -1,10 +1,15 @@
+# with help from
+# http://www.programcreek.com/python/example/57384/test.test_support.EnvironmentVarGuard
 
+from test.support import EnvironmentVarGuard
 import unittest
 
 class TestEnv(unittest.TestCase):
-    def test_one(self):
-        from django.conf import settings
-        settings.configure()
-        from siteapp import settings
-        print("SETTINGS " + settings.SECRET_KEY)
-        self.assertEqual(True,True)
+    def test_secret_key(self):
+        with EnvironmentVarGuard() as environ:
+            environ['SECRET_KEY'] = 'xoxo'
+            from django.conf import settings
+            settings.configure()
+            from siteapp import settings
+            
+            self.assertEqual(settings.SECRET_KEY,'xoxo')
