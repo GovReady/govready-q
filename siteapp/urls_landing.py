2ffe39f70dc7d07e12b974f14b243bbce9b6154b
Joshua Tauberer <jt@occams.info>
2016-08-05 15:24:06 -0400

    analytics on org subdomains now only aggregates events for that organization, and the route is now available on the landing domain too to show site-wide analytics
    fixes #136

--- siteapp/urls_landing.py
+++ siteapp/urls_landing.py
@@ -4,10 +4,14 @@ from django.contrib import admin
 admin.autodiscover()
 
 import siteapp.views_landing as views_landing
+import guidedmodules
 
 urlpatterns = [
     url(r"^$", views_landing.homepage),
     url(r"^about/", views_landing.aboutpage),
+
+    # analytis, which are available here and on organization subdomains
+    url(r'^tasks/analytics$', guidedmodules.views.analytics),
 
     # admin site
     url(r'^admin/', include(admin.site.urls)),
