0534ae2074835547db60ec271f5e641fc6cda1bd
Joshua Tauberer <jt@occams.info>
2016-08-08 16:16:51 -0400

    only use whitenoise when deplying in CloudFoundry
    The module is only installed in cf-requirements, so onnly run when VCAP_APPLICATION is present in the environment.
    
    Our nginx deployment doesn't need (and shouldn't use) whitenoise, so it wasn't enough to skip whitenoise just when running via Django's "runserver".

--- siteapp/wsgi.py
+++ siteapp/wsgi.py
@@ -3,6 +3,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "siteapp.settings")
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
-if 'runserver' not in sys.argv:
+if os.getenv('VCAP_APPLICATION'):
     from whitenoise.django import DjangoWhiteNoise
     application = DjangoWhiteNoise(application)
