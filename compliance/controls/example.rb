d3533756231d27d9fae8a300be7e66bbfac5658f
Peter Burkholder <pburkholder@pobox.com>
2016-08-16 12:24:15 -0400

    WIP working example from inspec

---
+++ compliance/controls/example.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+# copyright: 2015, Chef Software, Inc.
+# license: All rights reserved
+
+title '/tmp profile'
+
+# you add controls here
+control "tmp-1.0" do                        # A unique ID for this control
+  impact 0.7                                # The criticality, if this control fails.
+  title "Create /tmp directory"             # A human-readable title
+  desc "An optional description..."         # Describe why this is needed
+  tag data: "temp data"                     # A tag allows you to associate key information
+  tag "security"                            # to the test
+  ref "Document A-12", url: 'http://...'    # Additional references
+
+  describe file('/tmp') do                  # The actual test
+    it { should be_directory }
+  end
+end
+
+# you can also use plain tests
+describe file('/tmp') do
+  it { should be_directory }
+end
---
+++ compliance/controls/gordon.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+# copyright: 2016, Chef Software, Inc.
+# license: All rights reserved
+
+title 'Gordon Config Checks'
+
+# To pass the test, create the following file
+# ```bash
+# mkdir -p /tmp/gordon
+# cat <<EOF > /tmp/gordon/config.yaml
+# version: '1.0'
+# EOF
+# ```
+control 'gordon-1.0' do
+  impact 0.7
+  title 'Verify the version number of Gordon'
+  desc 'An optional description...'
+  tag 'gordon'
+  ref 'Gordon Requirements 1.0', uri: 'http://...'
+
+  # Test using the custom gordon_config Inspec resource
+  # Find the resource content here: ../libraries/
+  describe gordon_config do
+    it { should exist }
+    its('version') { should eq('1.0') }
+    its('file_size') { should <= 20 }
+    its('comma_count') { should eq 0 }
+  end
+
+  # Test the version again to showcase variables
+  g = gordon_config
+  g_path = g.file_path
+  g_version = g.version
+  describe file(g_path) do
+    its('content') { should match g_version }
+  end
+end
