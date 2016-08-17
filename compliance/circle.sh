d7e8d13bd84422b609aa372650aa03f48dfc8684
Peter Burkholder <pburkholder@pobox.com>
2016-08-17 16:45:48 -0400

    Circle should auto validate spaces after deploys

---
+++ compliance/circle.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# A wrapper script for running Compliance
+# from a circleci node
+
+set -euf -o pipefail -x
+
+# select the control to use based on branch
+BRANCH=${CIRCLE_BRANCH:-master}
+
+case $BRANCH in
+  master) space=dev;;
+  prod)   space=prod;;
+  *)      space=sandbox;;
+esac
+
+control=cf-$space-roles
+
+cf api https://api.run.pivotal.io
+cf auth $PWS_AUDITOR_USER $PWS_AUDITOR_PASS
+
+cf target -o GovReady -s $space
+
+cd compliance &&
+  bundle exec inspec exec . --controls $control
--- compliance/controls/cf.rb
+++ compliance/controls/cf.rb
@@ -13,12 +13,18 @@ control 'cf-prod-roles' do
 
   describe cf_space_roles('prod') do
     its('managers') {
-      should contain_exactly('pburkholder@govready.com','consulting@joshdata.me','gregelin@govready.com')
+      should contain_exactly(
+        'pburkholder@govready.com',
+        'consulting@joshdata.me',
+        'gregelin@govready.com'
+      )
     }
     its('developers') {
       should contain_exactly('secdevops+pivotalprodrelease@govready.com')
     }
-    its('auditors') { should be_empty }
+    its('auditors') {
+      should contain_exactly('secdevops+pivotalauditor@govready.com')
+    }
   end
 end
 
@@ -44,6 +50,36 @@ control 'cf-dev-roles' do
         'consulting@joshdata.me',
       )
     }
-    its('auditors') { should be_empty }
+    its('auditors') {
+      should contain_exactly('secdevops+pivotalauditor@govready.com')
+    }
+  end
+end
+
+control 'cf-sandbox-roles' do
+  impact 0.7
+  title 'Ensure roles for sandbox space is appropriate'
+  desc 'tb'
+  tag 'cf'
+  ref 'CF Requirements 1.0', uri: 'http://...'
+
+  describe cf_space_roles('dev') do
+    its('managers') {
+      should contain_exactly(
+        'pburkholder@govready.com',
+        'consulting@joshdata.me',
+        'gregelin@govready.com'
+      )
+    }
+    its('developers') {
+      should contain_exactly(
+        'secdevops+pivotaldevrelease@govready.com',
+        'pburkholder@govready.com',
+        'consulting@joshdata.me',
+      )
+    }
+    its('auditors') {
+      should contain_exactly('secdevops+pivotalauditor@govready.com')
+    }
   end
 end
