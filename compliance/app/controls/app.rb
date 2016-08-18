8b5a1ead1d04d9084eb122153bf04edd8a7f25ed
Peter Burkholder <pburkholder@pobox.com>
2016-08-18 09:51:25 -0400

    Works with empty vcap_services

--- compliance/app/controls/app.rb
+++ compliance/app/controls/app.rb
@@ -2,20 +2,21 @@
 # copyright: 2016, GovReady PBC
 # license: All rights reserved
 
-#title 'CloudFoundry Node Compliance'
+title 'CloudFoundry Node Compliance'
 
-control 'staging-info' do
+control 'papertrail-logging' do
   impact 0.7
-  title 'Ensure control works'
-  desc 'This control verifies operation of Inspec by checking for existence of a remote file'
+  title 'Papertrail Log Drain'
+  desc 'Each app instance should be configure with a papertrail log drain'
 
-  describe file('/home/vcap/staging_info.yml') do
-    it { should exist }
-  end
-  describe file('/etc/hosts') do
+  describe cf_vcap_services() do
     it { should exist }
   end
 end
 
 include_controls('os-hardening') do
+  skip_control "os-06" # Pivotal's Ubuntu incluces several SUID/SGID binaries
+  %w(01 05 06 07 08 09 10 11 17 18 20 21 22 23 24 25 26 27 28 30).each do |ctl|
+    skip_control "sysctl-#{ctl}"
+  end
 end
