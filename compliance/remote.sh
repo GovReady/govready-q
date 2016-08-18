a54c0963b8e8695b7e4496bfcc63d44ae00dae4a
Peter Burkholder <pburkholder@pobox.com>
2016-08-18 14:51:10 -0400

    Fix paths in shell scripts

--- compliance/remote.sh
+++ compliance/remote.sh
@@ -2,7 +2,6 @@
 
 # A wrapper script for running Compliance
 # from a circleci node against a deployed app container
-
 set -euf -o pipefail -x
 
 ssh_hostport=$(cf curl /v2/info | jq '.app_ssh_endpoint' -r )
@@ -12,12 +11,11 @@ ssh_guid=$(cf app govready-q --guid)
 ssh_pass=$(cf ssh-code)
 ssh_user="cf:$ssh_guid/0"
 
-control=staging-info
-#ssh -p $ssh_port cf:$ssh_guid/0@$ssh_host
+[ $(basename $PWD) = "compliance" ] || cd compliance
+
 bundle exec inspec exec ./app \
   -b ssh \
   --host=$ssh_host \
   --user=$ssh_user \
   --password $ssh_pass \
   --port=$ssh_port
-#  -t ssh://cf:$ssh_guid/0@$ssh_host --password $ssh_pass
