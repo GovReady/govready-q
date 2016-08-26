GovReady/govready-q

commit 271e64eba2ff356d3b6b21b12df89eb2996541dc

drop introduction ("/start") pages and instead treat 'introduction's as an implicit interstitial question that every other question depends on
This simplifies a lot of code that had to choose whether or not to go to the task start page or to a task question. Those links all now go to task questions.

The 'introduction' on projects is now converted into an implicit interstitial question and an ask-first dependency is added to all other questions so that it is necessarily asked first. This occurs in the load_modules management command.

The introduction page had two nav links, one to invite someone else to take over (this is on question pages too) and another one to return a task to the person who invited the user (which is nowhere else).

fixes #78

guidedmodules/views.py
@@ -23,10 +23,10 @@ def new_task(request):
     task = project.root_task.get_or_create_subtask(request.user, request.POST["question"])
 
     # Redirect.
-    return HttpResponseRedirect(task.get_absolute_url() + "/start")
+    return HttpResponseRedirect(task.get_absolute_url())
 
 @login_required
-def next_question(request, taskid, taskslug, intropage=None):
+def next_question(request, taskid, taskslug):
     # Get the Task.
     task = get_object_or_404(Task, id=taskid)
 
@@ -35,9 +35,6 @@ def next_question(request, taskid, taskslug, intropage=None):
 
     # Process form data.
     if request.method == "POST":
-        if intropage:
-            return HttpResponseForbidden()
-
         # does user have write privs?
         if not task.has_write_priv(request.user):
             return HttpResponseForbidden()
@@ -98,7 +95,7 @@ def next_question(request, taskid, taskslug, intropage=None):
                     title=m1.title)
 
                 answered_by_tasks = [t]
-                redirect_to = t.get_absolute_url() + "/start"
+                redirect_to = t.get_absolute_url()
 
             elif value == None:
                 # User is skipping this question.
@@ -189,7 +186,7 @@ def next_question(request, taskid, taskslug, intropage=None):
 
     # Redirect if slug is not canonical. We do this after checking for
     # read privs so that we don't reveal the task's slug to unpriv'd users.
-    if request.path != task.get_absolute_url() + (intropage or ""):
+    if request.path != task.get_absolute_url():
         return HttpResponseRedirect(task.get_absolute_url())
 
     # Display requested question.
@@ -205,14 +202,7 @@ def next_question(request, taskid, taskslug, intropage=None):
         "open_invitations": task.get_open_invitations(request.user),
     }
 
-    if intropage:
-        context.update({
-            "introduction": task.render_introduction(),
-            "source_invitation": task.invitation_history.filter(accepted_user=request.user).order_by('-created').first(),
-        })
-        return render(request, "module-intro.html", context)
-
-    elif not q:
+    if not q:
         # There is no next question - the module is complete.
         context.update({
             "output": task.render_output_documents(answered),

templates/question.html
@@ -151,7 +151,7 @@ Next Question: {{q.spec.title}}
                   <label>
                     <input name="value" value="{{t.id}}" type="radio" checked>
                     Use my answers in {{t.render_title}}
-                      (<a href="{{t.get_absolute_url}}{% if not t.is_finished %}/start{% endif %}">view{% if t.can_write %}/edit{% endif %}</a>)
+                      (<a href="{{t.get_absolute_url}}">view{% if t.can_write %}/edit{% endif %}</a>)
                   </label>
                 </div>
 

siteapp/views.py
@@ -21,8 +21,7 @@ def homepage(request):
     settings_task = request.user.get_settings_task()
     if not settings_task.is_finished():
         # First task: Fill out your account settings.
-        return HttpResponseRedirect(settings_task.get_absolute_url()
-            + "/start")
+        return HttpResponseRedirect(settings_task.get_absolute_url())
 
     else:
         # Ok, show user what they can do --- list the projects they

guidedmodules/management/commands/load_modules.py
@@ -67,6 +67,10 @@ class Command(BaseCommand):
         for m1 in self.get_module_spec_dependencies(spec):
             self.process_module(m1, processed_modules, path + [spec["id"]])
 
+        # Pre-process the module.
+
+        self.preprocess_module_spec(spec)
+
         # Ok now actually do the database update for this module...
 
         # Get the most recent version of this module in the database,
@@ -109,6 +113,19 @@ class Command(BaseCommand):
             if question.get("type") in ("module", "module-set"):
                 yield question.get("module-id")
 
+    def preprocess_module_spec(self, spec):
+        # 'introduction' fields are an alias for an interstitial
+        # question that all questions depend on.
+        if "introduction" in spec:
+            q = {
+                "id": "_introduction",
+                "title": "Introduction",
+                "type": "interstitial",
+                "prompt": spec["introduction"]["template"],
+            }
+            for q1 in spec.get("questions", []):
+                q1.setdefault("ask-first", []).append(q["id"])
+            spec.setdefault("questions", []).insert(0, q)
 
     def create_module(self, spec):
         # Create a new Module instance.

guidedmodules/models.py
@@ -187,7 +187,7 @@ class Task(models.Model):
         self.invitation_history.add(invitation)
 
     def get_invitation_redirect_url(self, invitation):
-        return self.get_absolute_url() + "/start"
+        return self.get_absolute_url()
 
     def get_open_invitations(self, user):
         # Return the open Invitations for transferring task ownership

guidedmodules/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 import guidedmodules.views
 
 urlpatterns = [
-    url(r'^(\d+)/([\w_-]+)(/start)?$', guidedmodules.views.next_question),
+    url(r'^(\d+)/([\w_-]+)$', guidedmodules.views.next_question),
     url(r'^start$', guidedmodules.views.new_task),
     url(r'^_change_state$', guidedmodules.views.change_task_state, name="task_change_state"),
 ]

templates/module-intro.html
@@ -1,93 +1,0 @@
-{% extends "base.html" %}
-
-{% block title %}
-{{task.title}}
-{% endblock %}
-
-{% block body %}
-
-<style>
-	.well p:last-child {
-		margin: 0;
-	}
-</style>
-
-<div class="row">
-	<div class="col-sm-push-2 col-sm-8">
-    <div style="margin-bottom: 45px" >
-      {% include "task-header.html" with col_width="4" col_active="start" %}
-    </div>
-
-        {% with source_invitation as inv %}
-          {% if inv %}
-            <div class="panel panel-warning">
-              <div class="panel-heading">Invitation from {{inv.from_user}}</div>
-              <div class="panel-body">
-                {{inv.text}}
-              </div>
-            </div>
-          {% endif %}
-        {% endwith %}
-
-		<div class="well well-lg">
-			{{introduction|safe}}
-		</div>
-
-		{% if task.is_started %}
-		<p>This module has already been started.</p>
-		{% endif %}
-
-		<a href="{{task.get_absolute_url}}" class="btn btn-primary">
-			{% if task.is_finished %}
-				Review answers &raquo;
-			{% elif task.is_started %}
-				Continue &raquo;
-			{% else %}
-				Let&rsquo;s get started &raquo;
-			{% endif %}
-		</a>
-
-    {% if task.can_transfer_owner %}
-		<p style="margin-top: 30px">
-            Or you can <a href="javascript:invite_user_to_take_over_module()">invite a colleague to edit this module</a>.
-            {% if source_invitation %}
-            Or you can <a href="javascript:return_to_requestor({{source_invitation.from_user.id}})">send it back to {{source_invitation.from_user}}</a>.
-            {% endif %}
-    </p>
-    {% endif %}
-	</div>
-</div>
-
-{% endblock %}
-
-
-{% block scripts %}
-<script>
-function invite_user_to_take_over_module() {
-  show_invite_modal(
-    '{{task.title|escapejs}}',
-    'Invite a colleague to answer this module?',
-    {{send_invitation|safe}},
-    'Can you give me a hand completing the module {{task.title|escapejs}} for {{task.project.title|escapejs}}?',
-    {
-      project: {{task.project.id}},
-      into_task_editorship: {{task.id}}
-    },
-    function() { window.location.reload() }
-  );
-}
-
-function return_to_requestor(user) {
-  show_invite_modal(
-    'Return This Module',
-    'Let the person that requested this module from you know that you are unable to complete it.',
-    {{send_invitation|safe}},
-    'I cannot answer {{task.title|escapejs}}.',
-    {
-      user_id: user,
-      project: {{task.project.id}},
-      into_task_editorship: {{task.id}}
-    });
-}
-</script>
-{% endblock %}

templates/project.html
@@ -143,20 +143,19 @@
                     {% if is_member %}
                       {% for t in rec.tasks %}
                         <div>
-                          <!-- <a href="{{t.get_absolute_url}}{% if not t.is_started %}/start {% endif %}"> -->
                             {% if rec.question.spec.type == "module-set" %}
                               {{t.render_title}} &mdash;
                             {% endif %}
                             {% if t.is_finished or not t.has_write_priv %}
-                              <a href="{{t.get_absolute_url}}{% if not t.is_started %}/start {% endif %}">
+                              <a href="{{t.get_absolute_url}}">
                                 <span class="glyphicon glyphicon-check"></span>
                                 Completed </a>
                             {% elif t.is_started %}
-                              <a href="{{t.get_absolute_url}}{% if not t.is_started %}/start {% endif %}">
+                              <a href="{{t.get_absolute_url}}">
                                 <span class="glyphicon glyphicon-edit"></span>
                               In Progress </a>
                             {% else %}
-                              <a href="{{t.get_absolute_url}}{% if not t.is_started %}/start {% endif %}">
+                              <a href="{{t.get_absolute_url}}">
                               <span class="glyphicon glyphicon-edit"></span>
                               Viewed </a>
                             {% endif %}

commit 3463a0a371ccf3248558a45b29e7e7e93d38cf66

the task-header's 'start module' (not 'answer questions') bar should be highlighted when the user hasnt yet answered any questions and is on an interstitial, which is the best way to know if a user is just starting the task
this got broke in 271e64eba2ff356d3b6b21b12df89eb2996541dc when we removed the actual start page

fixes #173

guidedmodules/views.py
@@ -372,6 +372,8 @@ def next_question(request, taskid, taskslug):
         # Construct the page.
         context.update({
             "DEBUG": settings.DEBUG,
+
+            "header_col_active": "start" if (len(answered.answers) == 0 and q.spec["type"] == "interstitial") else "questions",
             "q": q,
             "prompt": task.render_question_prompt(q),
             "history": taskq.get_history() if taskq else None,

templates/question.html
@@ -30,7 +30,7 @@ Next Question: {{q.spec.title}} | {{task.title}}
 
 <div class="row">
   <div class="col-sm-push-1 col-sm-9">
-    {% include "task-header.html" with col_width="4" col_active="questions" %}
+    {% include "task-header.html" with col_width="4" col_active=header_col_active %}
 
     {% if source_invitation %}
       <div style="margin-top: 1.5em">