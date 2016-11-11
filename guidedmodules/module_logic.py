GovReady/govready-q

commit 8daddf0b9787eb55491698ea9e277ec06a7d1747

import project data from JSON file
* added a button to the project settings tab
* decoupled validation from parsing UI form fields
* refactored the logic for saving answers - moved it from views.py to models.py so it can be re-used by JSON import
* and lots of additional deserialization logic

guidedmodules/module_logic.py
@@ -568,74 +568,219 @@ def run_impute_conditions(conditions, context):
             return (value,)
     return None
 
+class question_input_parser:
+    # Turns client-side input into the correct JSON-serializable
+    # Python data structure for a particular question type.
+    #
+    # The input is typically a string submitted by an <input> field,
+    # which is passed to this function by request.POST.getlist(),
+    # giving us an array of strings.
+    #
+    # But file-type questions are given by request.FILES.get(), which
+    # gives a Django UploadedFile instance.
+    #
+    # The output of the parse function is a JSON-serializable data
+    # structure that matches the structure that export_json/import_json
+    # functions use, because that's the format that the validator
+    # functions expect since the validator is used both for web input
+    # and imported input.
+    @classmethod
+    def parse(_class, question, value):
+        # Mangle value a bit.
+        if question.spec["type"] == "multiple-choice":
+            # A list of strings is what we want here.
+            pass
+
+        elif question.spec["type"] == "file":
+            # An UploadedFile instance is what we want here.
+            pass
+
+        else:
+            # For all other question types, turn the array of strings
+            # into a single string.
+            if len(value) > 1:
+                raise ValueError("Invalid input: Multiple values submitted.")
+            elif len(value) == 0:
+                raise ValueError("Invalid input: No value submitted.")
+            else:
+                value = value[0].strip()
+
+        # Get the parser function and run it on the value.
+        func = getattr(_class, "parse_" + question.spec["type"].replace("-", "_"))
+        return func(question, value)
+
+    # The textual question types don't need to be parsed. They come
+    # as strings and are stored as strings.
+    def parse_text(question, value):
+        return value
+    def parse_password(question, value):
+        return value
+    def parse_email_address(question, value):
+        return value
+    def parse_url(question, value):
+        return value
+    def parse_longtext(question, value):
+        return value
+    def parse_date(question, value):
+        return value
+
+    # Likewise for the simple choice types.
+    def parse_choice(question, value):
+        return value
+    def parse_yesno(question, value):
+        return value
+
+    def parse_multiple_choice(question, value):
+        # Comes in from the view function as an array of strings, which is what we want.
+        return value
+
+    def parse_integer(question, value):
+        # First parse it as a real using localization.
+        value = question_input_parser.parse_real(question, value)
+
+        # Then ensure is an integer.
+        if value != int(value):
+            raise ValueError("Invalid input. Must be a whole number.")
+
+        # Finally, convert data type.
+        return int(value)
+
+    def parse_real(question, value):
+        try:
+            # Use a locale to parse human input since it may have
+            # e.g. thousands-commas.
+            import locale
+            locale.setlocale(locale.LC_ALL, 'en_US.UTF-8') 
+            return locale.atof(value)
+        except ValueError:
+            # make a nicer error message
+            raise ValueError("Invalid input. Must be a number.")
+
+    def parse_file(question, value):
+        # This comes in as a Django File instance. Convert it to the
+        # data structure used for export/import.
+
+        # Otherwise, we get an UploadedFile instance.
+        import django.core.files.uploadedfile
+        if not isinstance(value, django.core.files.uploadedfile.UploadedFile):
+            raise ValueError("Invalid data type.")
+
+        # Pull the whole file into memory and return the JSON data structure.
+        # Encode the content as an array of (short) base-64 strings.
+        # TODO: Maybe check value.charset and do transcoding to utf-8?
+        import re
+        from base64 import b64encode
+        return {
+            "content": re.findall(".{64}", b64encode(value.read()).decode("ascii")),
+            "type": value.content_type, # as given by the client
+        }
+
+    def parse_module(question, value):
+        # handled by view function
+        return value
+
+    def parse_interstitial(question, value):
+        # interstitials have no actual answer - we should always
+        # get "" - and the internal value is always None.
+        if value != "":
+            raise ValueError("Invalid input.")
+        return None
+
+    def parse_external_function(question, value):
+        # the user doesn't answer these directly
+        if value != "":
+            raise ValueError("Invalid input.")
+        return None # doesn't matter
+
 class validator:
-    @staticmethod
-    def validate(question, value):
-        validate_func = getattr(validator, "validate_" + question.spec["type"].replace("-", "_"))
+    # Validate that an answer is of the right data type and meets the
+    # criteria of a question, and returns a normalized value.
+    @classmethod
+    def validate(_class, question, value):
+        validate_func = getattr(_class, "validate_" + question.spec["type"].replace("-", "_"))
         return validate_func(question, value)
 
     def validate_text(question, value):
+        if not isinstance(value, str):
+            raise ValueError("Invalid data type (%s)." % type(value))
+        value = value.strip()
         if value == "":
-            raise ValueError("empty")
+            raise ValueError("Value is empty.")
         return value
 
     def validate_password(question, value):
-        if value == "":
-            raise ValueError("empty")
+        # Run the same checks as text (data type is str, stripped, and is not empty).
+        value = validator.validate_text(question, value)
         return value
 
     def validate_email_address(question, value):
+        # Run the same checks as text (data type is str, stripped, and is not empty).
+        value = validator.validate_text(question, value)
+
+        # Then validate and normalize the value as an email address.
         import email_validator
         return email_validator.validate_email(value)["email"]
 
     def validate_url(question, value):
-        if value == "":
-            raise ValueError("empty")
+        # Run the same checks as text (data type is str, stripped, and is not empty).
+        value = validator.validate_text(question, value)
+
+        # Then validate and normalize the URL.
         from django.core.validators import URLValidator
-        validator = URLValidator()
+        urlvalidator = URLValidator()
         try:
-            validator(value)
+            urlvalidator(value)
         except:
             raise ValueError("That is not a valid web address.")
         return value
 
     def validate_longtext(question, value):
-        if value == "":
-            raise ValueError("empty")
+        # Run the same checks as text (data type is str, stripped, and is not empty).
+        value = validator.validate_text(question, value)
         return value
 
     def validate_date(question, value):
+        # Run the same checks as text (data type is str, stripped, and is not empty).
+        value = validator.validate_text(question, value)
+
         # Validate that we have a valid date in YYYY-MM-DD format. A client-side
         # tool should be responsible for ensuring that the user entry is translated
         # into this format.
         import re, datetime
-        if value == "":
-            raise ValueError("empty")
         m = re.match("(\d\d\d\d)-(\d\d)-(\d\d)$", value)
         if not m:
             raise ValueError("Date is not in YYYY-MM-DD format.")
+
         # Convert to ints, raising ValueError if they are not.
         try:
             year, month, date = [int(x) for x in m.groups()]
         except ValueError:
             raise ValueError("Date is not in YYYY-MM-DD format.")
+
         # Instantiate a datetime.date object. It will raise a ValueError if the
         # year, month, or day is out of range.
         datetime.date(year, month, date)
+
+        # Return the original string (not a datetime instance).
         return value
 
     def validate_choice(question, value):
+        if not isinstance(value, str):
+            raise ValueError("Invalid data type (%s)." % type(value))
         if value not in { choice['key'] for choice in question.spec["choices"] }:
             raise ValueError("invalid choice")
         return value
 
     def validate_yesno(question, value):
+        if not isinstance(value, str):
+            raise ValueError("Invalid data type (%s)." % type(value))
         if value not in ("yes", "no"):
             raise ValueError("invalid choice")
         return value
 
     def validate_multiple_choice(question, value):
-        # Comes in from the view function as an array.
+        if not isinstance(value, list):
+            raise ValueError("Invalid data type (%s)." % type(value))
         for item in value:
             if item not in { choice['key'] for choice in question.spec["choices"] }:
                 raise ValueError("invalid choice: " + item)
@@ -646,45 +791,51 @@ class validator:
         return value
 
     def validate_integer(question, value):
-        # First validate as a real so we don't have to duplicate those tests.
-        # We get back a float.
-        value = validator.validate_real(question, value)
-
-        # Then ensure is an integer.
-        if value != int(value):
-            raise ValueError("Invalid input. Must be a whole number.")
-
-        return int(value)
+        if not isinstance(value, int):
+            raise ValueError("Invalid data type (%s)." % type(value))
+        value = validator._validate_number(question, value)
+        return value
 
     def validate_real(question, value):
-        if value == "":
-            raise ValueError("empty")
+        if not isinstance(value, float):
+            raise ValueError("Invalid data type (%s)." % type(value))
+        value = validator._validate_number(question, value)
+        return value
 
-        try:
-            # Use a locale to parse human input since it may have
-            # e.g. thousands-commas.
-            import locale
-            locale.setlocale(locale.LC_ALL, 'en_US.UTF-8') 
-            value = locale.atof(value)
-        except ValueError:
-            # make a nicer error message
-            raise ValueError("Invalid input. Must be a number.")
-
+    @staticmethod
+    def _validate_number(question, value):
+        # This method is used on ints and floats.
         if "min" in question.spec and value < question.spec["min"]:
             raise ValueError("Must be at least %g." % question.spec["min"])
         if "max" in question.spec and value > question.spec["max"]:
             raise ValueError("Must be at most %g." % question.spec["max"])
-
         return value
 
     def validate_file(question, value):
-        # None is an acceptable value: It means to preserve
-        # an existing answer.
-        if value is None:
-            return value
+        # The JSON-serializable data structure for a file is a dict like:
+        #
+        # {
+        #   "content": [ array of Base64-encoded strings ],
+        #   "contentType": "text/plain",
+        # }
+        #
+        # Turn this into a Django ContentFile instance which is how we'll
+        # save it into the database.
 
-        # Otherwise, the file must have content.
-        if value.size == 0:
+        # Check data type.
+        if not isinstance(value, dict):
+            raise ValueError("Invalid data type (%s)." % type(value))
+        if not isinstance(value.get("content"), list):
+            raise ValueError("Invalid data type.")
+        if not isinstance(value.get("type"), str):
+            raise ValueError("Invalid data type.")
+
+        # Fetch content.
+        from base64 import b64decode
+        content = b64decode("".join(str(chunk) for chunk in value["content"]).encode("ascii"))
+
+        # The file must have content.
+        if len(content) == 0:
             raise ValueError("File is empty.")
 
         # If the file is expected to be an image, then load it to ensre it is
@@ -693,9 +844,10 @@ class validator:
         # the user. (TODO: Test that it actually drops XMP metadata.)
         if question.spec.get("file-type") == "image":
             # Load the image.
+            from io import BytesIO
             from PIL import Image
             try:
-                im = Image.open(value.file)
+                im = Image.open(BytesIO(content))
             except:
                 raise ValueError("That's not an image file.")
 
@@ -707,34 +859,35 @@ class validator:
                 size = imspec["max-size"]
                 im.thumbnail(( size.get("width", im.size[0]), size.get("width", im.size[1])  ))
 
-            # Write the image back to a new Django ContentFile instance.
-            from io import BytesIO
+            # Write the image back to a new buffer.
             buf = BytesIO()
             im.save(buf, "PNG")
+            content = buf.getvalue()
 
-            from django.core.files.base import ContentFile
-            value = ContentFile(buf.getvalue())
-            value.name = "image.png" # needs a name for the storage backend?
-
-
+        # Turn it into a Django ContentFile instance.
+        from django.core.files.base import ContentFile
+        value = ContentFile(content)
+        value.name = "image.png" # needs a name for the storage backend?
         return value
 
     def validate_module(question, value):
-        # handled by view function
+        # handled by caller function
+        return value
+
+    def validate_module_set(question, value):
+        # handled by caller function
         return value
 
     def validate_interstitial(question, value):
         # interstitials have no actual answer - we should always
-        # get "".
-        if value != "":
-            raise ValueError("Invalid input.")
-        return None # store it as null
+        # store None.
+        if value is not None:
+            raise ValueError("Invalid data type (%s)." % type(value))
+        return value
 
     def validate_external_function(question, value):
-        # the user doesn't answer these directly
-        if value != "":
-            raise ValueError("Invalid input.")
-        return None # doesn't matter
+        # Any data structure is OK.
+        return value
 
 def get_question_choice(question, key):
     for choice in question.spec["choices"]:

guidedmodules/models.py
@@ -7,7 +7,7 @@ from jsonfield import JSONField
 from collections import OrderedDict
 import uuid
 
-from .module_logic import ModuleAnswers, render_content
+from .module_logic import ModuleAnswers, render_content, validator
 from siteapp.models import User, Project, ProjectMembership
 
 class Module(models.Model):
@@ -453,6 +453,135 @@ class Task(models.Model):
             ])
             )
 
+    @staticmethod
+    def import_json(data, deserializer, for_question):
+        def do_deserialize():
+            # Gets or creates a Task instance corresponding to the Task encoded
+            # in the data. The Task must be an answer to a TaskQuestion instance,
+            # given as for_question, which determines the Module that the task
+            # uses.
+
+            # Basic validation.
+            if not isinstance(data, dict):
+                deserializer.log("Data format error.")
+                return
+            if not isinstance(data.get("id"), str):
+                deserializer.log("Data format error.")
+                return
+
+            # If there's a Task in the system with the UUID found in the incoming
+            # data, use that, assuming the user has write permission on it.
+            task = Task.objects.filter(uuid=data["id"]).first()
+            if task:
+                if not task.has_write_priv(deserializer.user):
+                    deserializer.log("%s (%s): You do not have permission to update that task."
+                        % (task.title, data['id']))
+                    return None
+
+            else:
+                # The UUID doesn't correspond with anything in the database, so
+                # create a new task.
+                task = Task.create(
+                    parent_task_answer=for_question, # for instrumentation only, doesn't go into Task instance
+                    editor=deserializer.user,
+                    project=for_question.task.project,
+                    module=for_question.question.answer_type_module,
+                    title=for_question.question.answer_type_module.title,
+                    uuid=data['id'], # preserve the UUID from the incoming data
+                    )
+
+            # Recursively fill in the answers to the newly created task.
+            task.import_json_update(data, deserializer)
+            return task
+
+        # If the serialized data is a reference to something we've already deserialized
+        # and imported, then return the Task directly. Otherwise call do_deserialize to
+        # do the actual work of deserializing & updating a Task.
+        return deserializer.deserializeOnce(data, do_deserialize)
+
+    def import_json_update(self, data, deserializer):
+        # Imports/overwrites/merges question answers from the given data structure
+        # into this Task, and into any sub-Tasks that are answers to questions.
+        # The user is known only to have write permission to this Task.
+
+        # Since this method is called directly on a Project root task, we must
+        # repeat some of the same validation that occurs in Task.import_json.
+        if not isinstance(data, dict):
+            deserializer.log("Data format error.")
+            return
+
+        # Overwrite metadata if the fields are present, i.e. allow for
+        # missing or empty fields, which will preserve the existing metadata we have.
+        self.title = data.get("title") or self.title
+        self.save(update_fields=["title"])
+
+        # We don't chek that the module listed in the data matches the module
+        # this Task actually uses in the database. We don't have a way to test
+        # equality and if there's a mismatch there's nothing we can really do.
+        # Instead we just carefully validate the incoming answers.
+
+        my_name = "%s (%s)" % (self.title, data.get("id") or "no UUID")
+        did_update_any_questions = False
+
+        # Merge answers to questions.
+
+        if "answers" in data:
+            if not isinstance(data["answers"], dict):
+                deserializer.log("Data format error.")
+                return
+
+            # Loop through the key-value pairs.
+            for qkey, answer in data["answers"].items():
+                # Get the ModuleQuestion instance for this question.
+                q = self.module.questions.filter(key=qkey).first()
+                if q is None:
+                    deserializer.log("Task %s question %s ignored (not a valid question ID)." % (my_name, qkey))
+                    continue
+
+                # For logging.
+                qname = q.spec.get("title") or qkey
+
+                # Ensure the data type matches the current question specification.
+                if q.spec["type"] != answer.get("questionType"):
+                    deserializer.log("Task %s question %s ignored (question type mismatch)." % (my_name, qname))
+                    continue
+
+                # Validate the answer value (check data type, range), if it is answered.
+                # (Any question can be skipped.)
+                if answer.get("value") is not None:
+                    try:
+                        value = validator.validate(q, answer["value"])
+                    except ValueError as e:
+                        deserializer.log("Task %s question %s ignored: %s" % (my_name, qname, str(e)))
+                        continue
+                else:
+                    # The value is None so we don't validate. Any question
+                    # can be answered with None, meaning it was skipped.
+                    value = None
+
+                # Get or create the TaskAnswer instance for this question.
+                taskanswer, _ = TaskAnswer.objects.get_or_create(
+                    task=self,
+                    question=q,
+                )
+
+                # Prepare the fields for saving.
+                prep_fields = TaskAnswerHistory.import_json_prep(taskanswer, value, deserializer)
+                if prep_fields is None:
+                    deserializer.log("Task %s question %s has been skipped because a sub-task could not be used." % (my_name, qname))
+                    continue
+
+                value, answered_by_tasks, answered_by_file = prep_fields
+
+                # And save the answer.
+                if taskanswer.save_answer(value, answered_by_tasks, answered_by_file, deserializer.user):
+                    deserializer.log("Task %s question %s was updated." % (my_name, qname))
+                    did_update_any_questions = True
+                
+            if not did_update_any_questions:
+                deserializer.log("Task %s had no new answers to save." % (my_name,))
+
+
 class TaskAnswer(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="answers", help_text="The Task that this TaskAnswer is a part of.")
     question = models.ForeignKey(ModuleQuestion, on_delete=models.PROTECT, help_text="The question (within the Task's Module) that this TaskAnswer is answering.")
@@ -481,6 +610,12 @@ class TaskAnswer(models.Model):
     def get_current_answer(self):
         # The current answer is the one with the highest primary key.
         return self.answer_history.order_by('-id').first()
+
+    def has_answer(self):
+        ans = self.get_current_answer()
+        if ans and not ans.cleared:
+            return True
+        return False
 
     def get_history(self):
         from discussion.models import reldate
@@ -535,6 +670,65 @@ class TaskAnswer(models.Model):
             del item["date"] # not JSON serializable
 
         return history
+
+    def clear_answer(self, user):
+        ans = self.get_current_answer()
+        if ans is None or ans.cleared:
+            # No answer record yet or the record has already marked the question
+            # as cleared.
+            return False
+
+        # Store a new TaskAnswerHistory record with the cleared flag set.
+        TaskAnswerHistory.objects.create(
+            taskanswer=self,
+            answered_by=user,
+            stored_value=None,
+            answered_by_file=None,
+            cleared=True)
+
+        # kick the Task and TaskAnswer's updated field
+        self.task.save(update_fields=[])
+        self.save(update_fields=[])
+        return True
+
+    def save_answer(self, value, answered_by_tasks, answered_by_file, user):
+        # Save the answer and return True if the answer was changed (vs was not
+        # updated because the value matched the value of the existing answer).
+        current_answer = self.get_current_answer()
+
+        # Check if the answer is changing. If not, return False.
+
+        def are_files_same():
+            if answered_by_file is None and current_answer.answered_by_file.name == "":
+                # No files in either case -- so the file field is the same.
+                return True
+            if answered_by_file is None or current_answer.answered_by_file.name == "":
+                # One but not both are null, so there is a change.
+                return False
+            # Both have content -- check if the content matches.
+            return answered_by_file.read() == current_answer.answered_by_file.read()
+
+        if current_answer and not current_answer.cleared \
+            and value == current_answer.stored_value \
+            and set(answered_by_tasks) == set(current_answer.answered_by_task.all()) \
+            and are_files_same():
+            return False
+
+        # The answer is new or changing. Create a new record for it.
+        answer = TaskAnswerHistory.objects.create(
+            taskanswer=self,
+            answered_by=user,
+            stored_value=value,
+            answered_by_file=answered_by_file)
+        for t in answered_by_tasks:
+            answer.answered_by_task.add(t)
+
+        # kick the Task and TaskAnswer's updated field
+        self.task.save(update_fields=[])
+        self.save(update_fields=[])
+
+        # Return True to indicate we saved something.
+        return True
 
     # required to attach a Discussion to it
     @property
@@ -641,6 +835,10 @@ class TaskAnswerHistory(models.Model):
     def export_json(self, serializer):
         # Exports this TaskAnswerHistory's value to a JSON-serializable Python data structure.
         # Called via siteapp.Project::export_json.
+        #
+        # The data structure should match the output format of module_logic.question_input_parser
+        # and the input format expected by module_logic.validator.validate() because the
+        # validate function is called during import.
 
         # Get the answer's current value.
         value = self.get_value()
@@ -651,9 +849,22 @@ class TaskAnswerHistory(models.Model):
             if q.spec["type"] == "module":
                 # It's a ModuleAnswers instance -- serialize the Task itself.
                 value = value.task.export_json(serializer)
+            
             elif q.spec["type"] == "module-set":
                 # It's an array of ModuleAnswers instances.
                 value = [x.task.export_json(serializer) for x in value]
+            
+            elif q.spec["type"] == "file":
+                # Although self.get_value() returns useful data, it's not the export
+                # format because it doesn't include the file content (only a URL to it).
+                # Add the file content to it. It's other important field is 'type' which
+                # holds the MIME type.
+                import re
+                from base64 import b64encode
+                value.update({
+                    "content": re.findall(".{64}", b64encode(self.answered_by_file.read()).decode("ascii")),
+                })
+
             else:
                 # Any value that we might have stored in the database is definitely
                 # JSON-serializabile because that's how it's stored. Special question
@@ -668,6 +879,53 @@ class TaskAnswerHistory(models.Model):
             ("answeredAt", self.created.isoformat()),
             ("value", value),
         ])
+
+    @staticmethod
+    def import_json_prep(taskanswer, value, deserializer):
+        # Given a JSON-serializable data structure produced by export_json, prepare
+        # it for saving as a new answer by returning content for our stored_value,
+        # answered_by_tasks, and answered_by_file database fields.
+
+        answered_by_tasks = set()
+        answered_by_file = None
+
+        # Skipped questions.
+        if value is None:
+            return value, answered_by_tasks, answered_by_file
+
+        # Special question types that aren't stored in the stored_value field.
+        q = taskanswer.question
+        if q.spec["type"] in ("module", "module-set"):
+            # These questions are stored
+
+            # Make module-type questions look like module-set questions
+            # so we can handle the rest the same.
+            if q.spec["type"] == "module":
+                value = [value]
+
+            # For each sub-taks, get or create the Task instance (and
+            # recurse to update its answers). Task.import_json can return
+            # None, and we handle that below.
+            for task in value:
+                task = Task.import_json(task, deserializer, taskanswer)
+                answered_by_tasks.add(task)
+
+            # Skip updating this question if there was any error fetching
+            # a Task.
+            if None in answered_by_tasks:
+                # Could not import.
+                return None
+
+            # Reset this part.
+            value = None
+
+        elif q.spec["type"] == "file":
+            # The value is a Django ContentFile and is stored in a dedicated field
+            answered_by_file = value
+            value = None
+
+        return value, answered_by_tasks, answered_by_file
+
 
 class InstrumentationEvent(models.Model):
     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)

commit 12127aa0ee0cae15035d89577553704f4f71b469

file upload was broken by refactoring in 8daddf0b9787eb55491698ea9e277ec06a7d1747

guidedmodules/module_logic.py
@@ -671,7 +671,7 @@ class question_input_parser:
         import re
         from base64 import b64encode
         return {
-            "content": re.findall(".{64}", b64encode(value.read()).decode("ascii")),
+            "content": re.findall(".{1,64}", b64encode(value.read()).decode("ascii")),
             "type": value.content_type, # as given by the client
         }
 