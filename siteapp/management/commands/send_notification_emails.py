9e787fe2b04ffab0922c6c87b123c35e7c88f1f5
Joshua Tauberer <jt@occams.info>
2017-01-01 17:07:52 -0500

    move the exclusive_process function to a new pypi package I created named exclusiveprocess

--- siteapp/management/commands/send_notification_emails.py
+++ siteapp/management/commands/send_notification_emails.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 
 import time, uuid
 
+from exclusiveprocess import Lock
+
 from notifications.models import Notification
 
 class Command(BaseCommand):
@@ -13,7 +15,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Ensure this process doesn't run multiple times concurrently.
-        exclusive_process("q_send_notification_emails")
+        Lock(die=True).forever()
 
         if len(args) == 1 and args[0] == "forever":
             # Loop forever.
@@ -94,77 +96,3 @@ class Command(BaseCommand):
         notif.recipient.notifemails_last_at = timezone.now()
         notif.recipient.save(update_fields=['notifemails_last_notif_id', 'notifemails_last_at'])
 
-def exclusive_process(name):
-    # Based on https://github.com/mail-in-a-box/mailinabox/blob/master/management/utils.py
-    ######################################################################################
-
-    # Ensure that this process, named `name`, does not execute multiple
-    # times concurrently by writing our process ID to a global file.
-
-    import os, sys, atexit
-
-    pidfile = '/tmp/%s.pid' % name
-    mypid = os.getpid()
-
-    # This function will be used with atexit to clear the pid file when
-    # the program terminates.
-    def clear_my_pid():
-        os.unlink(pidfile)
-
-    def is_pid_valid(pid):
-        """Checks whether a pid is a valid process ID of a currently running process."""
-        # adapted from http://stackoverflow.com/questions/568271/how-to-check-if-there-exists-a-process-with-a-given-pid
-        import os, errno
-        if pid <= 0: raise ValueError('Invalid PID.')
-        try:
-            os.kill(pid, 0)
-        except OSError as err:
-            if err.errno == errno.ESRCH: # No such process
-                return False
-            elif err.errno == errno.EPERM: # Not permitted to send signal
-                return True
-            else: # EINVAL
-                raise
-        else:
-            return True
-
-    # Attempt to get a lock on ourself so that the concurrency check
-    # itself is not executed in parallel.
-    with open(__file__, 'r+') as flock:
-        # Try to get a lock. This blocks until a lock is acquired. The
-        # lock is held until the flock file is closed at the end of the
-        # with block.
-        os.lockf(flock.fileno(), os.F_LOCK, 0)
-
-        # While we have a lock, look at the pid file. First attempt
-        # to write our pid to a pidfile if no file already exists there.
-        try:
-            with open(pidfile, 'x') as f:
-                # Successfully opened a new file. Since the file is new
-                # there is no concurrent process. Write our pid.
-                f.write(str(mypid))
-                atexit.register(clear_my_pid)
-
-        except FileExistsError:
-            # The pid file already exixts, but it may contain a stale
-            # pid of a terminated process.
-            with open(pidfile, 'r+') as f:
-                # Read the pid in the file.
-                try:
-                    existing_pid = int(f.read().strip())
-                except ValueError:
-                    pass
-                else:
-                    # Check if the pid in it is valid, and if so print a
-                    # message and exit.
-                    if is_pid_valid(existing_pid):
-                        print("Another %s is already running (pid %d)." % (name, existing_pid), file=sys.stderr)
-                        sys.exit(1)
-
-                # The file didn't have a valid pid, so overwrite the file
-                # with our pid.
-                f.seek(0)
-                f.write(str(mypid))
-                f.truncate()
-                atexit.register(clear_my_pid)
- 
