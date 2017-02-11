GovReady/govready-q

commit 7e16cb2bf1ce67c5bcb52d562e76e248692279a6

add a new Folder model for grouping Projects
* new model class with a M2M to Project & migration to create folders for all existing non-system projects
* update the project-list template to show folders and projects within folders
* ability to rename folders, new permissions for folder admins, and documented permissions
* for module-type questions, list modules in the same folder as the current project first, when listing projects use the project's name rather than the module's instance-name, and don't redundantly show the project title a second time

siteapp/views.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django.utils import timezone
 from django.db import transaction
 
-from .models import User, Project, Invitation
+from .models import User, Folder, Project, Invitation
 from guidedmodules.models import Module, Task, ProjectMembership
 from discussion.models import Discussion
 
@@ -17,56 +17,42 @@ from .notifications_helpers import *
 def homepage(request):
     from allauth.account.forms import LoginForm
     return render(request, "index.html", {
-        "has_projects": len(get_user_projects(request)) > 0,
+        "has_projects": len(Project.get_projects_with_read_priv(request.user, request.organization)) > 0,
         "login_form": LoginForm,
     })
 
-def get_user_projects(request):
-    # Show user what they can do --- list the projects they
-    # are involved with.
-    projects = set()
-
-    if not request.user.is_authenticated:
-        return projects
-
-    # Add all of the Projects the user is a member of within the Organization
-    # that the user is on the subdomain of.
-    for pm in ProjectMembership.objects\
-        .filter(project__organization=request.organization, user=request.user)\
-        .select_related('project'):
-        projects.add(pm.project)
-        if pm.is_admin:
-            # Annotate with whether the user is an admin of the project.
-            pm.project.user_is_admin = True
-
-    # Add projects that the user is the editor of a task in, even if
-    # the user isn't a team member of that project.
-    for task in Task.get_all_tasks_readable_by(request.user, request.organization)\
-        .order_by('-created')\
-        .select_related('project'):
-        projects.add(task.project)
-
-    # Add projects that the user is participating in a Discussion in
-    # as a guest.
-    for d in Discussion.objects.filter(organization=request.organization, guests=request.user):
-        if d.attached_to is not None: # because it is generic there is no cascaded delete and the Discussion can become dangling
-            projects.add(d.attached_to.task.project)
-
-    # Don't show system projects. They're displayed separately.
-    system_projects = set(p for p in projects if p.is_organization_project or p.is_account_project)
-    projects -= system_projects
-
-    # Sort.
-    projects = sorted(projects, key = lambda x : x.updated, reverse=True)
-
-    return projects
-
 def project_list(request):
-    projects = get_user_projects(request)
+    # Get the projects.
+    projects = Project.get_projects_with_read_priv(request.user, request.organization)
     for p in projects:
         p.open_tasks = p.get_open_tasks(request.user) # for the template
+
+    # Collate into folders. Folders are accessible to a user
+    # just when they can see a Project within it, so we go
+    # backwards from Projects to Folders.
+    folders = list(Folder.objects.filter(projects__in=projects).distinct())
+
+    for folder in folders:
+        # Set an attribute on the Folder with a list of
+        # projects that the user has access to.
+        projects_in_folder = set(folder.projects.all())
+        folder.accessible_projects = [p for p in projects if p in projects_in_folder]
+
+        # Mark folders that the user is an admin of, i.e. can rename it and
+        # see all projects within it.
+        folder.is_admin = (request.user in folder.get_admins())
+
+        # If the user is an admin and there are projects in the folder the
+        # user can't see, mark that too.
+        if folder.is_admin:
+            folder.num_hidden_projects = len(projects_in_folder - set(folder.accessible_projects))
+
+    # Sort the folders by the sort order of the first project
+    # in each folder.
+    folders.sort(key = lambda folder : [projects.index(p) for p in folder.accessible_projects])
+
     return render(request, "project_list.html", {
-        "projects": projects,
+        "folders": folders,
         "any_have_members_besides_me": ProjectMembership.objects.filter(project__in=projects).exclude(user=request.user),
     })
 
@@ -75,6 +61,9 @@ def new_project(request):
     from django.forms import ModelForm, ChoiceField, RadioSelect, MultipleChoiceField, \
         CheckboxSelectMultiple, CharField, Textarea
     from django.core.exceptions import ValidationError
+
+    # What Folders can the new Project be added to?
+    folders = list(Folder.get_all_folders_admin_of(request.user))
 
     # Get the list of project modules that this user has access to.
     # The built-in server-side form validation will ensure that the user can
@@ -122,7 +111,15 @@ def new_project(request):
             help_texts = {
                 'title': 'Give your web property, application or other IT system a descriptive name.',
             }
+
+        if len(folders) > 0:
+            add_to_folder = ChoiceField(
+                choices=[("", "Add to New Folder")] + [(f.id, f) for f in folders],
+                required=False,
+                label="Add to a folder?")
+
         module_id = ChoiceField(choices=project_modules, label="What do you need to do?", widget=RadioSelect)
+
         if len(invitable_users) > 0:
             invite_org_users = MultipleChoiceField(label='Select existing users to add to project',
                 choices=sorted([
@@ -131,6 +128,7 @@ def new_project(request):
                     key = lambda choice : choice[1]),
                 widget=CheckboxSelectMultiple,
                 required=False)
+
         invite_by_email = CharField(label="Invite others to this project",
             help_text="Separate email addresses by new lines, spaces, or commas.",
             widget=Textarea(attrs={'rows': '2', 'placeholder': ''}),
@@ -160,6 +158,19 @@ def new_project(request):
                     project=project,
                     user=request.user,
                     is_admin=True)
+
+                # add to a folder that the user has privilege to, or a new folder
+                # (The form field isn't shown if there are no existing folders
+                # (get returns None), and if the user wants to create a new folder
+                # the value is the empty string, otherwise it is a string containing
+                # a primary key (positive integer).)
+                if form.cleaned_data.get("add_to_folder"):
+                    folder = Folder.objects.get(id=form.cleaned_data["add_to_folder"])
+                else:
+                    folder = Folder.objects.create(
+                        organization=project.organization,
+                        title=project.title)
+                folder.projects.add(project)
 
                 # add other invited users
                 # (the form field is absent in some cases so must check we have the field)
@@ -365,6 +376,19 @@ def project(request, project_id):
         "tabs": list(tabs.values()),
         "action_buttons": action_buttons,
     })
+
+def rename_folder(request):
+    # Get the folder.
+    folder = get_object_or_404(Folder, id=request.POST.get("folder"))
+
+    # Validate that the user can rename it.
+    if request.user not in folder.get_admins():
+        return JsonResponse({ "status": "error", "message": "Not authorized." })
+
+    # Update.
+    folder.title = request.POST.get("title")
+    folder.save()
+    return JsonResponse({ "status": "ok" })
 
 def project_admin_login_post_required(f):
     # Wrap the function to do authorization and change arguments.

guidedmodules/views.py
@@ -387,12 +387,14 @@ def next_question(request, taskid, taskslug):
             # Sort the instances:
             #  first: the current answer, if any
             #  then: tasks defined in the same project as this task
+            #  later: tasks defined in projects in the same folder as this task's project
             #  last: everything else by reverse update date
             now = timezone.now()
             current_answer = answer.answered_by_task.first() if answer else None
             answer_tasks = sorted(answer_tasks, key = lambda t : (
                 not (t == current_answer),
                 not (t.project == task.project),
+                not (set(t.project.contained_in_folders.all()) & set(task.project.contained_in_folders.all())),
                 now-t.updated,
                 ))
 

guidedmodules/models.py
@@ -412,6 +412,10 @@ class Task(models.Model):
         return self.project.get_members()
 
     def render_title(self):
+        # Project root tasks derive their rendered title from the
+        # project title and don't use the instance-name.
+        if self == self.project.root_task:
+            return self.project.title
         return self.render_simple_string("instance-name", self.module.title)
 
     def render_invitation_message(self):

siteapp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 import django.contrib.auth.admin as contribauthadmin
 
-from .models import User, Organization, Project, ProjectMembership
+from .models import User, Organization, Folder, Project, ProjectMembership
 
 def all_user_fields_still_exist(fieldlist):
     for f in fieldlist:
@@ -113,8 +113,15 @@ class OrganizationAdmin(admin.ModelAdmin):
 
     actions = [add_me_as_admin, populate_test_organization]
 
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created')
+    raw_id_fields = ('organization',)
+    readonly_fields = ('projects', 'extra')
+
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('project', 'root_task', 'created')
+    raw_id_fields = ('organization', 'root_task')
+    readonly_fields = ('extra',)
     def project(self, obj):
         return obj.organization_and_title()
 
@@ -126,6 +133,7 @@ class ProjectMembershipAdmin(admin.ModelAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Folder, FolderAdmin)
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(ProjectMembership, ProjectMembershipAdmin)
 

siteapp/migrations/0013_folder.py
@@ -1,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Generated by Django 1.10.5 on 2017-02-08 19:43
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+import jsonfield.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('siteapp', '0012_auto_20161221_1705'),
+    ]
+
+
+    def create_a_folder_for_every_project(apps, schema_editor):
+        # Create a Folder for every non-special project.
+        Folder = apps.get_model("siteapp", "Folder")
+        Project = apps.get_model("siteapp", "Project")
+        for project in Project.objects.filter(
+            is_account_project=False,
+            is_organization_project=None, # None is used in place of False, see the model
+            ):
+            folder = Folder.objects.create(
+                organization=project.organization,
+                title=project.title,
+            )
+            folder.projects.add(project)
+
+    operations = [
+        migrations.CreateModel(
+            name='Folder',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(help_text='The title of this Project.', max_length=256)),
+                ('created', models.DateTimeField(auto_now_add=True, db_index=True)),
+                ('updated', models.DateTimeField(auto_now=True, db_index=True)),
+                ('extra', jsonfield.fields.JSONField(blank=True, help_text='Additional information stored with this object.')),
+                ('organization', models.ForeignKey(help_text='The Organization that this project belongs to.', on_delete=django.db.models.deletion.CASCADE, related_name='folders', to='siteapp.Organization')),
+                ('projects', models.ManyToManyField(help_text='The Projects that are listed within this Folder.', related_name='contained_in_folders', to='siteapp.Project')),
+            ],
+        ),
+        migrations.RunPython(create_a_folder_for_every_project, reverse_code=lambda apps, schema : None),
+    ]

siteapp/models.py
@@ -189,8 +189,47 @@ class Organization(models.Model):
         profile = profile_task.get_answers().as_dict()
         return profile.get("logo")
 
+class Folder(models.Model):
+    """A folder is a collection of Projects."""
+
+    organization = models.ForeignKey(Organization, related_name="folders", help_text="The Organization that this project belongs to.")
+
+    title = models.CharField(max_length=256, help_text="The title of this Project.")
+
+    projects = models.ManyToManyField("Project", related_name="contained_in_folders", help_text="The Projects that are listed within this Folder.")
+
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated = models.DateTimeField(auto_now=True, db_index=True)
+    extra = JSONField(blank=True, help_text="Additional information stored with this object.")
+
+    def __str__(self):
+        # For the admin, notification strings
+        return self.title
+
+    def __repr__(self):
+        # For debugging.
+        return "<Folder %d %s>" % (self.id, self.title[0:30])
+
+    def get_admins(self):
+        # Get all of the Users with admin privs on the folder --- which
+        # is the set of users that have admin privs on any project within
+        # it.
+        ret = User.objects.none()
+        for project in self.projects.all():
+            ret |= project.get_admins()
+        return ret.distinct()
+
+    @staticmethod
+    def get_all_folders_admin_of(user):
+        # Get all Folders that this user has privs to rename and add
+        # new Projects to.
+        return Folder.objects\
+            .filter(projects__members__user=user, projects__members__is_admin=True)\
+            .distinct()
 
 class Project(models.Model):
+    """"A Project is a set of Tasks rooted in a Task whose Module's type is "project". """
+
     organization = models.ForeignKey(Organization, related_name="projects", help_text="The Organization that this project belongs to.")
     is_organization_project = models.NullBooleanField(default=None, help_text="Each Organization has one Project that holds Organization membership privileges and Organization settings (in its root Task). In order to have a unique_together constraint with Organization, only the values None (which need not be unique) and True (which must be unique to an Organization) are used.")
 
@@ -308,6 +347,51 @@ class Project(models.Model):
 
         # Return sorted by username.
         return sorted(participants.items(), key = lambda kv : kv[0].username)
+
+    @staticmethod
+    def get_projects_with_read_priv(user, organization):
+        # Gets all projects a user has read priv to, excluding
+        # account and organization profile projects, and sorted
+        # in reverse chronological order by modified date.
+
+        projects = set()
+
+        if not user.is_authenticated:
+            return projects
+
+        # Add all of the Projects the user is a member of within the Organization
+        # that the user is on the subdomain of.
+        for pm in ProjectMembership.objects\
+            .filter(project__organization=organization, user=user)\
+            .select_related('project'):
+            projects.add(pm.project)
+            if pm.is_admin:
+                # Annotate with whether the user is an admin of the project.
+                pm.project.user_is_admin = True
+
+        # Add projects that the user is the editor of a task in, even if
+        # the user isn't a team member of that project.
+        from guidedmodules.models import Task
+        for task in Task.get_all_tasks_readable_by(user, organization)\
+            .order_by('-created')\
+            .select_related('project'):
+            projects.add(task.project)
+
+        # Add projects that the user is participating in a Discussion in
+        # as a guest.
+        from discussion.models import Discussion
+        for d in Discussion.objects.filter(organization=organization, guests=user):
+            if d.attached_to is not None: # because it is generic there is no cascaded delete and the Discussion can become dangling
+                projects.add(d.attached_to.task.project)
+
+        # Don't show system projects.
+        system_projects = set(p for p in projects if p.is_organization_project or p.is_account_project)
+        projects -= system_projects
+
+        # Sort.
+        projects = sorted(projects, key = lambda x : x.updated, reverse=True)
+
+        return projects
 
     def get_open_tasks(self, user):
         # Get all tasks that the user might want to continue working on

siteapp/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     url(r'^projects/(\d+)/__import$', views.import_project_data, name="import_project_data"),
     url(r'^projects/(\d+)/(?:[\w\-]+)$', views.project), # must be last because regex matches some previous URLs
     url(r'^projects/(\d+)/(?:[\w\-]+)/start$', views.begin_project),
+    url(r'^__rename_folder$', views.rename_folder, name="rename_folder"),
 
     # invitations
     url(r'^invitation/_send$', views.send_invitation, name="send_invitation"),

templates/project_list.html
@@ -10,19 +10,25 @@ Home
 
 {% block head %}
 <style>
-.project {
-  margin: 0em 0;
+.folder {
   border-top:1px solid rgb(240,240,240); 
   border-bottom:1px solid rgb(240,240,240); 
-  padding: 8px 0px; 0px 4px;
 }
-  .project a {
-    font-weight: bold;
+  .folder .rename_folder {
+    margin: 0 1em;
+    color: #777;
+    cursor: pointer;
+    font-size: 13px;
   }
-  .project a:hover { text-decoration: none; }
-  .project a:hover span { text-decoration: underline; }
-  .project a:hover span.glyphicon { text-decoration: none; }
-.project:hover{
+  h2 { font-size: 115%; font-weight: bold; margin: 1em 0; }
+  .folder .project { margin: 1em 0; }
+  h3 { font-size: 100%; font-weight: bold; margin: 0; }
+  h2 .label, h3 .label { margin-left: .75em; font-size: 12px; }
+  .folder .project h2 { margin: 0; }
+  .folder a:hover { text-decoration: none; }
+  .folder a:hover span { text-decoration: underline; }
+  .folder a:hover span.glyphicon { text-decoration: none; }
+.folder:hover{
   background-color: rgb(250,250,250); 
 }
 .glyphicon-book { color: #888; }
@@ -36,7 +42,7 @@ Home
   Projects/Systems
 </h1>
     
-    {% if projects|length == 0 %}
+    {% if folders|length == 0 %}
       <p>Your IT systems and projects will be listed on this page. Start by creating your first IT system or project.</p>
     {% else %}
       <p>You are helping with these {{request.organization.name}} systems and projects.</p>
@@ -47,9 +53,25 @@ Home
 </div>
 
 <!-- List projects -->
-{% for project in projects %}
-<div class="project">
-  <a href="{% if project %}{{project.get_absolute_url}}{% else %}/projects/system{% endif %}">
+{% for folder in folders %}
+<div class="folder" data-folder-id="{{folder.id}}">
+  {% if folder.accessible_projects|length != 1 %}
+  <h2>
+    <span class="folder_name">{{folder.title}}</span>
+    {% if folder.is_admin %}
+    <span class="rename_folder" onclick="rename_folder(this);"><span class="glyphicon glyphicon-pencil"></span></span>
+    {% endif %}
+  </h2>
+  {% endif %}
+
+  {% for project in folder.accessible_projects %}
+  <div class="project">
+  {% if folder.accessible_projects|length != 1 %}
+  <h3>
+  {% else %}
+  <h2>
+  {% endif %}
+  <a href="{{project.get_absolute_url}}">
     <span class="glyphicon glyphicon-hdd" style="color:rgb(19,144,50)"></span>
     <span>{{project.title}}</span>
 
@@ -60,6 +82,12 @@ Home
       {% endif %}
     {% endwith %}
   </a>
+  {% if folder.accessible_projects|length != 1 %}
+  </h3>
+  {% else %}
+  </h2>
+  {% endif %}
+
 
   {# what kind of project is this #}
   <div class="small" style="margin-top: .5em">
@@ -84,8 +112,39 @@ Home
       </div>
     {% endif %}
   {% endwith %}
+
+  </div>
+  {% endfor %}
+
+  {% if folder.is_admin and folder.num_hidden_projects > 0 %}
+  <p><i>There are {{folder.num_hidden_projects}} other assessment{{folder.num_hidden_projects|pluralize}} in this folder that you do not have permission to see.</i></p>
+  {% endif %}
 </div>
 {% endfor %}
 
 
+{% endblock %}
+
+
+{% block scripts %}
+<script>
+function rename_folder(elem) {
+  var folder = $(elem).parents(".folder");
+  var folder_id = folder.data("folder-id");
+  var folder_name = folder.find(".folder_name");
+  var new_title = prompt("Rename this folder?", folder_name.text());
+  if (!new_title) return;
+  ajax_with_indicator({
+   url: '{% url "rename_folder" %}',
+   method: "POST",
+   data: {
+    folder: folder_id,
+    title: new_title
+   },
+   success: function() {
+     folder_name.text(new_title);
+   }
+  });
+}
+</script>
 {% endblock %}

commit 72ade6890f459c3051166db2d41c5d72e1deb643

homepage crashed when logged out, broken by 7e16cb2b

siteapp/views.py
@@ -17,7 +17,8 @@ from .notifications_helpers import *
 def homepage(request):
     from allauth.account.forms import LoginForm
     return render(request, "index.html", {
-        "has_projects": len(Project.get_projects_with_read_priv(request.user, request.organization)) > 0,
+        "has_projects": (len(Project.get_projects_with_read_priv(request.user, request.organization)) > 0)
+            if hasattr(request, 'organization') else False,
         "login_form": LoginForm,
     })
 