b2ad9cedb989b2352c97cf80a63455a1bb2613fa
Joshua Tauberer <jt@occams.info>
2017-05-05 17:38:53 -0400

    add management command app_store_catalog to dump the available apps to stdout

---
+++ guidedmodules/management/commands/app_store_catalog.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+from guidedmodules.models import ModuleSource
+from guidedmodules.module_sources import MultiplexedAppStore
+
+class Command(BaseCommand):
+    help = 'Prints a list of all apps in the configured app stores.'
+
+    def handle(self, *args, **options):
+        with MultiplexedAppStore(ms for ms in ModuleSource.objects.all()) as store:
+            for app in store.list_apps():
+                print(app.store.source.namespace, app.name)
+
+
+
