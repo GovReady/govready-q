GovReady/govready-q

commit cfaf86b8f206c9efdf530202a994041beef037e4

normalize Module.key by renaming to Module.module_name and removing the source namespace and AppInstance appname from the start of the value so that it just holds the Module's relative path from the app directory so that the field isn't redundant with the fields in related tables
For old Module instances that are not tied to a AppInstance, leave the field's value as it was.

guidedmodules/views.py
@@ -735,7 +735,7 @@ def authoring_edit_reload_app(request, task):
     with AppStore.create(task.module.source) as store:
         for app in store.list_apps():
             # Only update using the app that provided this Task's Module.
-            if not (task.module.key+"/").startswith(task.module.source.namespace + "/" + app.name + "/"):
+            if app.appname != task.module.app.appname:
                 continue
 
             # Import.

siteapp/views.py
@@ -393,7 +393,8 @@ def start_app(app_catalog_info, organization, user, folder, task, q):
             # is just a placeholder.
             module = Module()
             module.source = module_source
-            module.key = app_catalog_info["key"]
+            # TODO module.app = None
+            module.module_name = "app"
             module.spec = dict(app_catalog_info)
             module.spec["type"] = "project"
             module.spec["is_app_stub"] = True

fixtures/modules/system/organization/app.yaml
@@ -1,6 +1,6 @@
 id: app
 title: About the Organization
-type: project
+type: system-project
 hidden: true
 version: 1
 long_description: >

guidedmodules/admin.py
@@ -15,11 +15,10 @@ class AppInstanceAdmin(admin.ModelAdmin):
 	list_filter = ('source',)
 
 class ModuleAdmin(admin.ModelAdmin):
-	list_display = ('id', 'source', 'app_', 'module_', 'visible', 'superseded_by', 'created')
+	list_display = ('id', 'source', 'app_', 'module_name', 'visible', 'superseded_by', 'created')
 	list_filter = ('source',)
 	raw_id_fields = ('app', 'superseded_by', 'assets')
 	def app_(self, obj): return "{} [{}]".format(obj.app.appname, obj.app.id) if obj.app else "(not in an app)"
-	def module_(self, obj): return obj.key[len(obj.source.namespace+"/"+obj.app.appname+"/"):] if obj.app else obj.key
 
 class ModuleQuestionAdmin(admin.ModelAdmin):
 	raw_id_fields = ('module', 'answer_type_module')

guidedmodules/management/commands/create_fixture.py
@@ -1,73 +1,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
-from django.db import transaction
-
-import sys
-
-from siteapp.models import User, Organization, Project, ProjectMembership
-from guidedmodules.models import Module, Task
-
-class Command(BaseCommand):
-    help = 'Creates a fixture that includes an organization, user, and modules.'
-
-    def add_arguments(self, parser):
-        parser.add_argument('projects', nargs="+", type=str)
-
-    def handle(self, *args, **options):
-        # Don't actually save to the database the intermediate
-        # objects we need to create to make a fixture.
-        class RollItBack(Exception): pass
-        try:
-            with transaction.atomic():
-                self.create_fixture(options)
-                raise RollItBack()
-        except RollItBack:
-            # Let the transaction roll back silently.
-            pass
-
-    def create_fixture(self, options):
-        # Create a user.
-        user = User()
-        user.username = "test"
-        user.set_password("1234")
-        user.save()
-
-        # Create an organization.
-        org = Organization.create(
-            name="Test Organization",
-            subdomain="test",
-            allowed_modules=options["projects"],
-            admin_user=user)
-
-        # All Module and ModuleQuestion instances that are needed.
-        # If the Module has questions that depend on other modules,
-        # include those in the fixture too, recursively.
-        modules = []
-
-        def add_module(m):
-            if m in modules: return
-            print(m, file=sys.stderr)
-            modules.append(m)
-            for mq in m.questions.all():
-                modules.append(mq)
-                if mq.answer_type_module:
-                    add_module(mq.answer_type_module)
-
-        required_projects = ["system/account_settings_project", "system/organization/app"]
-
-        for prj_module_key in required_projects + options["projects"]:
-            try:
-                m = Module.objects.get(key=prj_module_key, visible=True, superseded_by=None)
-            except Module.DoesNotExist:
-                print("Invalid module ID", prj_module_key, file=sys.stderr)
-                return
-            add_module(m)
-
-        # Ok, save.
-        from django.core import serializers
-        print(serializers.serialize("json",
-            [user,
-            org, org.get_organization_project(), org.get_organization_project().root_task,
-            pm] + modules,
-            indent=2))
-

guidedmodules/migrations/0030_module_key_rename_module_name.py
@@ -1,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+# For any Module with an AppInstance, chop of the ModuleSource.namespace
+# and AppInstance.appname from the Module key.
+def forwards_func(apps, schema_editor):
+    Module = apps.get_model("guidedmodules", "Module")
+    db_alias = schema_editor.connection.alias
+    for m in Module.objects.using(db_alias).exclude(app=None)\
+        .select_related("source", "app"):
+        if m.module_name.startswith(m.source.namespace+"/"+m.app.appname+"/"):
+            m.module_name = m.module_name[len(m.source.namespace)+1+len(m.app.appname)+1:]
+        m.save(using=db_alias)
+
+# For any Module with an AppInstance, add the ModuleSource.namespace
+# and AppInstance.appname to the Module key.
+def reverse_func(apps, schema_editor):
+    Module = apps.get_model("guidedmodules", "Module")
+    db_alias = schema_editor.connection.alias
+    Module.objects.using(db_alias)
+    for m in Module.objects.using(db_alias).exclude(app=None)\
+        .select_related("source", "app"):
+        m.module_name = m.source.namespace + "/" + m.app.appname + "/" + m.module_name
+        m.save(using=db_alias)
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('guidedmodules', '0029_auto_20170920_1443'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='module',
+            old_name='key',
+            new_name='module_name',
+        ),
+        migrations.AlterField(
+            model_name='module',
+            name='module_name',
+            field=models.SlugField(help_text='A slug-like identifier for the Module that is unique within the AppInstance app.', max_length=200),
+        ),
+        migrations.AlterUniqueTogether(
+            name='module',
+            unique_together=set([('app', 'module_name')]),
+        ),
+        migrations.RunPython(forwards_func, reverse_func),
+    ]

guidedmodules/models.py
@@ -56,7 +56,7 @@ class Module(models.Model):
     source = models.ForeignKey(AppSource, related_name="modules", on_delete=models.CASCADE, help_text="The source of this module definition.")
     app = models.ForeignKey(AppInstance, null=True, related_name="modules", on_delete=models.CASCADE, help_text="The AppInstance that this Module is a part of. Null for legacy Modules created before we had this field.")
 
-    key = models.SlugField(max_length=200, db_index=True, help_text="A slug-like identifier for the Module.")
+    module_name = models.SlugField(max_length=200, help_text="A slug-like identifier for the Module that is unique within the AppInstance app.")
 
     visible = models.BooleanField(default=True, db_index=True, help_text="Whether the Module is offered to users.")
     superseded_by = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL, help_text="When a Module is superseded by a new version, this points to the newer version.")
@@ -67,21 +67,22 @@ class Module(models.Model):
     created = models.DateTimeField(auto_now_add=True, db_index=True)
     updated = models.DateTimeField(auto_now=True, db_index=True)
 
+    class Meta:
+        unique_together = [
+            ("app", "module_name"),
+        ]
+
     def __str__(self):
         # For the admin.
-        return "%s [%d]" % (self.key, self.id)
+        return "%s %s [%d]" % (self.app, self.module_name, self.id)
 
     def __repr__(self):
         # For debugging.
-        return "<Module [%d] %s%s %s>" % (self.id, "" if not self.superseded_by else "(old) ", self.key, self.spec.get("title", "<No Title>")[0:30])
+        return "<Module [%d] %s%s %s (%s)>" % (self.id, "" if not self.superseded_by else "(old) ", self.module_name, self.spec.get("title", "<No Title>")[0:30], self.app)
 
     def save(self):
         if self.source != self.app.source: raise ValueError("Module source != app.source.")
         super(Module, self).save()
-
-    @property
-    def parent_path(self):
-        return "/".join(self.key.split("/")[:-1])
 
     @property
     def title(self):
@@ -113,9 +114,9 @@ class Module(models.Model):
         from collections import OrderedDict
         return serializer.serializeOnce(
             self,
-            "module:" + self.key, # a preferred key, doesn't need to be unique here
+            "module:" + self.app.source.namespace + "/" + self.app.appname + "/" + self.module_name, # a preferred key, doesn't need to be unique here
             lambda : OrderedDict([  # "lambda :" makes this able to be evaluated lazily
-                ("key", self.key),
+                ("key", self.module_name),
                 ("created", self.created.isoformat()),
                 ("modified", self.updated.isoformat()),
         ]))
@@ -219,13 +220,9 @@ class Module(models.Model):
         # Get the string that you would put in a YAML file to reference the
         # target module. target must be in get_referenceable_modules.
         # This is the inverse of validate_module_specification.resolve_relative_module_id.
-        root_path = self.source.namespace + "/" + self.app.appname + "/"
-        if target.key.startswith(root_path):
-            # If the target is in the same virtual directory or a subdirectory,
-            # use a virtual path.
-            return target.key[len(root_path):]
-        else:
+        if self.app != target.app:
             raise ValueError("Cannot reference %s from %s." % (target, self))
+        return target.module_name
     def serialize_to_disk(self):
         # Write out the in-memory module specification to disk!
         assert self.source.spec["type"] == "local" and self.source.spec["path"]
@@ -244,7 +241,7 @@ class Module(models.Model):
                 qspec["module-id"] = self.getReferenceTo(q.answer_type_module)
 
             spec["questions"].append(qspec)
-        fn = os.path.join(self.source.spec["path"], self.key[len(self.source.namespace)+1:]) + ".yaml"
+        fn = os.path.join(self.source.spec["path"], self.app.appname, self.module_name + ".yaml")
         with open(fn, "w") as f:
             f.write(rtyaml.dump(spec))
 
@@ -314,11 +311,12 @@ class ModuleQuestion(models.Model):
         unique_together = [("module", "key")]
 
     def __str__(self):
-        return "%s[%d].%s" % (self.module.key, self.module.id, self.key)
+        # For the admin.
+        return "%s[%d].%s" % (self.module, self.module.id, self.key)
 
     def __repr__(self):
         # For debugging.
-        return "<ModuleQuestion [%d] %s.%s (%s)>" % (self.id, self.module.key, self.key, repr(self.module))
+        return "<ModuleQuestion [%d] %s.%s (%s)>" % (self.id, self.module.module_name, self.key, repr(self.module))
 
     def choices_as_csv(self):
         # Helper method for module authoring.

guidedmodules/module_logic.py
@@ -862,7 +862,7 @@ class ModuleAnswers(object):
                             doc_name = "at index " + str(self.index)
                             if "title" in self.document:
                                 doc_name = repr(self.document["title"]) + " (" + doc_name + ")"
-                        doc_name = "%s output document %s" % (self.module_answers.module.key, doc_name)
+                        doc_name = "%s output document %s" % (self.module_answers.module.module_name, doc_name)
 
                         # Try to render it.
                         try:

commit 8d34a0dd2283be0a5fa22879d33a9f234fea89b5

reload app was broken by cfaf86b8

guidedmodules/views.py
@@ -735,7 +735,7 @@ def authoring_edit_reload_app(request, task):
     with AppStore.create(task.module.source) as store:
         for app in store.list_apps():
             # Only update using the app that provided this Task's Module.
-            if app.appname != task.module.app.appname:
+            if app.name != task.module.app.appname:
                 continue
 
             # Import.