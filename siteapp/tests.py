GovReady/govready-q

commit 863712f0b73ea3ce3f4dbf6674c4a57640258889

add an invitation button to the project page breadcrumbs bar

templates/project.html
@@ -128,11 +128,18 @@
 {% endblock %}
 
 {% block breadcrumbs_right %}
+<div style="margin-top: -4px">
 {% if not project.is_account_project or project.is_deletable %}
-  <button id="show-project-settings" class="btn btn-sm" onclick="$('#project_settings').modal();" style="margin-top: -4px">
+  <button id="show-project-settings" class="btn btn-sm" onclick="$('#project_settings').modal();">
     <i class="glyphicon glyphicon-wrench"></i> Assessment Settings</a>
   </button>
 {% endif %}
+{% if send_invitation.can_add_invitee_to_team %}
+  <button id="show-project-settings" class="btn btn-sm" onclick="invite_user_into_project()">
+    <i class="glyphicon glyphicon-plus"></i> <i class="glyphicon glyphicon-user"></i></a>
+  </button>
+{% endif %}
+</div>
 {% endblock %}
 
 {% block body %}

templates/base.html
@@ -45,9 +45,7 @@
               #context-bar .breadcrumb a {
                 color: #777;
               }
-            .glyphicon-book { color: #888; }
             .glyphicon-globe { color: #888; }
-            .glyphicon-plus { color: #FFF; }
             .btn { white-space: normal; font-size: inherit; }
             .btn.btn-sm { font-size: 12px; }
             footer { margin-top:60px;

commit 58966c803d25cb8da868d23e4fbe862726144371

fix duplicated DOM id (broken by 863712f0b73ea3ce3f4dbf6674c4a57640258889) which confused tests, and switch the tests to use the main invite button and not the one inside the settings modal

siteapp/tests.py
@@ -367,9 +367,8 @@ class GeneralTests(OrganizationSiteFunctionalTests):
             var_sleep(1)
 
         # Test an invitation to that project.
-        self.click_element("#show-project-settings")
+        self.click_element("#show-project-invite")
         var_sleep(.5) # modal fades in
-        self.click_element("#invite-user-to-project")
 
         # Test an invalid email address.
         start_invitation("example")
@@ -377,7 +376,7 @@ class GeneralTests(OrganizationSiteFunctionalTests):
         self.assertInNodeText("The email address is not valid.", "#global_modal") # make sure we get a stern message.
         self.click_element("#global_modal button") # dismiss the warning.
         var_sleep(.5)
-        self.click_element("#invite-user-to-project") # Re-open the invite box.
+        self.click_element("#show-project-invite") # Re-open the invite box.
 
         do_invitation("test+project@q.govready.com")
         self.assertRegex(self.browser.title, "I want to answer some questions on Q") # user is on the project page

templates/project.html
@@ -123,12 +123,12 @@
 
 {% block action_buttons %}
     {% if not project.is_account_project or project.is_deletable %}
-      <button id="show-project-settings" class="btn btn-sm" onclick="$('#project_settings').modal();">
+      <button class="btn btn-sm" onclick="$('#project_settings').modal();">
         <i class="glyphicon glyphicon-cog"></i> Settings</a>
       </button>
     {% endif %}
     {% if send_invitation.can_add_invitee_to_team %}
-      <button id="show-project-settings" class="btn btn-sm" onclick="invite_user_into_project()">
+      <button id="show-project-invite" class="btn btn-sm" onclick="invite_user_into_project()">
         <i class="glyphicon glyphicon-user"></i><i class="glyphicon glyphicon-plus"></i>
         Invite
       </button>