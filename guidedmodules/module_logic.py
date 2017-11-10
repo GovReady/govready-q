GovReady/govready-q

commit ce9e5950b22b2385e8fe2feec01eadcc5a805b90

first pass at showing review/approval and answer metadata in template rendering output by automatically wrapping variable substitutions in HTML tags that hold metadata attributes

guidedmodules/module_logic.py
@@ -119,7 +119,7 @@ def evaluate_module_state(current_answers, required, parent_context=None):
     # cleared from the context's cache for each question because each question sees
     # a different set of dependencies.
     impute_context_parent = TemplateContext(
-        ModuleAnswers(current_answers.module, current_answers.task, {}), lambda q, v : str(v),
+        ModuleAnswers(current_answers.module, current_answers.task, {}), lambda q, a, v : str(v),
         parent_context=parent_context)
 
     # Visitor function.
@@ -455,7 +455,7 @@ def render_content(content, answers, output_format, source, additional_context={
         # For other values we perform standard HTML escaping.
 
         if template_format == "text":
-            def escapefunc(question, value):
+            def escapefunc(question, answerobj, value):
                 # Don't perform any escaping.
                 return value
     
@@ -562,11 +562,12 @@ def render_content(content, answers, output_format, source, additional_context={
         raise ValueError("Invalid template format encountered: %s." % template_format)
 
 
-def HtmlAnswerRenderer(question, value):
+def HtmlAnswerRenderer(question, answerobj, value):
+    import html
     if question is None or question.spec["type"] != "longtext":
         # Regular text fields just get escaped.
-        import html
-        return html.escape(value)
+        value = html.escape(value)
+        wrappertag = "span"
     else:
         # longtext fields are rendered into the output
         # using CommonMark. Escape initial <'s so they
@@ -577,7 +578,23 @@ def HtmlAnswerRenderer(question, value):
         if value.startswith("<"): value = "\\" + value
         import CommonMark
         parsed = CommonMark.Parser().parse(value)
-        return CommonMark.HtmlRenderer({ "safe": True }).render(parsed)
+        value = CommonMark.HtmlRenderer({ "safe": True }).render(parsed)
+        wrappertag = "div"
+    return """<{tag} class='question-answer'
+      data-module='{module}'
+      data-question='{question}'
+      data-answered-by='{answered_by}'
+      data-answered-on='{answered_on}'
+      data-reviewed='{reviewed}'
+      >{value}</{tag}>""".format(
+        tag=wrappertag,
+        module=html.escape(question.module.spec['title']),
+        question=html.escape(question.spec["title"]),
+        answered_by=html.escape(str(answerobj.answered_by)) if answerobj else "(value was imputed)",
+        answered_on=html.escape(answerobj.created.strftime("%c")) if answerobj else "(n/a)",
+        reviewed=str(answerobj.reviewed) if answerobj else "",
+        value=value,
+    )
 
 
 def get_all_question_dependencies(module):
@@ -841,8 +858,9 @@ class TemplateContext(Mapping):
         question = self._module_questions.get(item)
         if question:
             # The question might or might not be answered. If not, its value is None.
-            answer = self.module_answers.as_dict().get(item, None)
-            return RenderedAnswer(self.module_answers.task, question, answer, self)
+            self.module_answers.as_dict() # trigger lazy-loading
+            answerobj, answervalue = self.module_answers.answertuples.get(item, (None, None, None, None))[2:4]
+            return RenderedAnswer(self.module_answers.task, question, answerobj, answervalue, self)
 
         # The context also provides the project and organization that the Task belongs to,
         # and other task attributes, assuming the keys are not overridden by question IDs.
@@ -913,7 +931,7 @@ class RenderedProject(TemplateContext):
     def as_raw_value(self):
         return self.project.title
     def __html__(self):
-        return self.escapefunc(None, self.as_raw_value())
+        return self.escapefunc(None, None, self.as_raw_value())
 
 class RenderedOrganization(TemplateContext):
     def __init__(self, organization, parent_context=None):
@@ -927,12 +945,13 @@ class RenderedOrganization(TemplateContext):
     def as_raw_value(self):
         return self.organization.name
     def __html__(self):
-        return self.escapefunc(None, self.as_raw_value())
+        return self.escapefunc(None, None, self.as_raw_value())
 
 class RenderedAnswer:
-    def __init__(self, task, question, answer, parent_context):
+    def __init__(self, task, question, answerobj, answer, parent_context):
         self.task = task
         self.question = question
+        self.answerobj = answerobj
         self.answer = answer
         self.parent_context = parent_context
         self.escapefunc = parent_context.escapefunc
@@ -956,7 +975,7 @@ class RenderedAnswer:
             value = str(self.answer)
 
         # And in all cases, escape the result.
-        return self.escapefunc(self.question, value)
+        return self.escapefunc(self.question, self.answerobj, value)
 
     @property
     def text(self):
@@ -1009,7 +1028,7 @@ class RenderedAnswer:
                 self.value = value
                 self.ra = ra
             def __html__(self):
-                return self.ra.escapefunc(self.ra.question, self.value)
+                return self.ra.escapefunc(self.ra.question, self.ra.answerobj, self.value)
         return SafeString(value, self)
 
     @property
@@ -1097,6 +1116,7 @@ class RenderedAnswer:
                     "type": "choice",
                     "choices": self.question.spec["choices"],
                     }),
+                self.answerobj,
                 ans, self.parent_context)
                 for ans in self.answer)
         

guidedmodules/tests.py
@@ -493,7 +493,7 @@ class RenderTests(TestCaseWithFixtureData):
         # test that we get the same thing if we use an impute condition with value-mode: expression,
         # but since the test is only given its string output convert the impute condition value
         # to a string
-        def escapefunc(question, value):
+        def escapefunc(question, answerobj, value):
             # ignores longtext rendering
             return value
         context = TemplateContext(answers, escapefunc) # parallels evaluate_module_state

siteapp/static/js/main.js
@@ -11,6 +11,14 @@ $(function() {
   // Make all tab navs responsive.
   $(".nav-tabs, .tab-content").addClass('responsive');
   fakewaffle.responsiveTabs(['xs', 'sm'])
+
+  // Make question answers show popovers with additional information.
+  $('.question-answer').popover({
+    content: function() {
+      return "Answered by " + this.getAttribute("data-answered-by") + " on " + this.getAttribute("data-answered-on")
+        + ". The question was: " + this.getAttribute("data-question") + " in " + this.getAttribute("data-module") + ".";
+    }
+  })
 })
 
 function textarea_ctrlenter_handler(e) {

siteapp/views.py
@@ -614,7 +614,7 @@ def project_list_all_answers(request, project):
             if not is_answered: continue # skip questions that have no answers
             if not a: continue # skip imputed answers
             if q.spec["type"] in ("interstitial", "module"): continue # skip question types that display awkwardly
-            value_display = RenderedAnswer(task, q, value, tc).__html__()
+            value_display = RenderedAnswer(task, q, a, value, tc).__html__()
             section["answers"].append((q, a, value_display))
         
         if section["answers"]:

templates/base.html
@@ -58,6 +58,20 @@
               font-size: .75em;
               color: #888;
             }
+
+            .question-answer[data-reviewed="0"] {
+              background-color: #d9534f;
+              color: white;
+              padding: 1px .33em;
+            }
+            .question-answer[data-reviewed="1"] {
+              border: 1px solid #DD7;
+              padding: 1px 3px;
+            }
+            .question-answer[data-reviewed="2"] {
+              border: 1px solid #DD7;
+              padding: 1px 3px;
+            }
         </style>
         <link rel="stylesheet" href="{% static "vendor/bootstrap/css/bootstrap-theme.min.css" %}">
         <link rel="stylesheet" href="{% static "vendor/fontawesome/css/font-awesome.min.css" %}">

commit 8ca18c42e0bab1ce977688701d4a47d42c393d53

fix '{% for choice in multiple_choice_question %}' iteration's rendering of the choice when answer metadata pop-ups are enabled
Broken by ce9e5950 which added the pop-ups for answer metadata.

Added tests.

Fixes #315.

guidedmodules/module_logic.py
@@ -1275,16 +1275,25 @@ class RenderedAnswer:
             return (None for _ in [])
 
         if self.question_type == "multiple-choice":
-            # Iterate by creating a RenderedAnswer for each choice key
-            # with a made-up Question instance.
+            # Iterate by creating a RenderedAnswer for each selected choice,
+            # with a made-up temporary Question instance that has the same
+            # properties as the actual multiple-choice choice but whose
+            # type is a single "choice".
             from .models import ModuleQuestion
             return (
-                RenderedAnswer(None, ModuleQuestion(spec={
-                    "type": "choice",
-                    "choices": self.question.spec["choices"],
-                    }),
-                self.answerobj,
-                ans, self.parent_context)
+                RenderedAnswer(
+                    self.task,
+                    ModuleQuestion(
+                        module=self.question.module,
+                        key=self.question.key,
+                        spec={
+                            "type": "choice",
+                            "title": self.question.spec['title'],
+                            "prompt": self.question.spec['prompt'],
+                            "choices": self.question.spec["choices"],
+                        }),
+                    self.answerobj,
+                    ans, self.parent_context)
                 for ans in self.answer)
         
         elif self.question_type == "module-set":

guidedmodules/tests.py
@@ -363,8 +363,8 @@ class RenderTests(TestCaseWithFixtureData):
 
 
     def test_render_choice_questions(self):
-        def test(*args):
-            self._test_render_single_question_md("question_types_choice", *args)
+        def test(*args, **kwargs):
+            self._test_render_single_question_md("question_types_choice", *args, **kwargs)
 
         from html import escape
 
@@ -386,6 +386,10 @@ class RenderTests(TestCaseWithFixtureData):
         test("q_multiple_choice.text", ["choice1", "choice3"], "Choice 1, Choice 3")
         test("q_multiple_choice", None, escape("<multiple-choice>"), None) # is actually the question's title, not its type, and {{...}} differently than in an impute condition
         test("q_multiple_choice.text", None, escape("<not answered>"))
+        test("q_multiple_choice", ["choice1", "choice3"], "[choice1][choice3]",
+            template="{% for choice in q_multiple_choice %}[{{choice}}]{% endfor %}")
+        test("q_multiple_choice", None, "", # not answered appears as nothing selected
+            template="{% for choice in q_multiple_choice %}[{{choice}}]{% endfor %}")
 
     def test_render_numeric_questions(self):
         def test(*args):
@@ -488,7 +492,7 @@ class RenderTests(TestCaseWithFixtureData):
         test("organization", "My Supreme Organization")
         test("project", "The Singleton Project")
 
-    def _test_render_single_question_md(self, module, expression, value, expected, expected_impute_value="__NOT__PROVIDED__", answers=None):
+    def _test_render_single_question_md(self, module, expression, value, expected, expected_impute_value="__NOT__PROVIDED__", answers=None, template=None):
         # Render the "{{question}}" or "{{question.text}}" using the given module.
 
         # create a ModuleAnswers instance that provides context for evaluating the template
@@ -499,11 +503,11 @@ class RenderTests(TestCaseWithFixtureData):
                 key: (m.questions.get(key=key), True, None, value) # if expression looks like "id.text" just use "id" here to set the answer
             })
 
-        # render the template. Wrap `expression` in "{{...}}"
+        # render the template. Wrap `expression` in "{{...}}" if template isn't provided.
         actual = render_content(
             {
                 "format": "markdown",
-                "template": "{{%s}}" % expression,
+                "template": template or ("{{%s}}" % expression),
             },
             answers,
             "html",
@@ -525,7 +529,7 @@ class RenderTests(TestCaseWithFixtureData):
         render_content(
             {
                 "format": "markdown",
-                "template": "{{%s}}" % expression,
+                "template": template or ("{{%s}}" % expression),
             },
             answers,
             "html",
@@ -533,14 +537,18 @@ class RenderTests(TestCaseWithFixtureData):
             show_answer_metadata=True,
         )
 
-        # test that we get the same thing if we use an impute condition with value-mode: expression,
-        # but since the test is only given its string output convert the impute condition value
-        # to a string
+        # Test that we get the same thing if we use an impute condition with `value-mode: expression`
+        # or, if template is provided, `value-mode: template`. For `value-mode: expression`, convert
+        # the imputed value to a string since the test is only given its string form.
         def escapefunc(question, task, is_answered, answerobj, value):
             # ignores longtext rendering
             return value
         context = TemplateContext(answers, escapefunc) # parallels evaluate_module_state
-        actual = run_impute_conditions([{ "condition": "1", "value": expression, "value-mode": "expression" }], context)
+        if not template:
+            impute_condition = { "value": expression, "value-mode": "expression" }
+        else:
+            impute_condition = { "value": template, "value-mode": "template" }
+        actual = run_impute_conditions([impute_condition], context)
         self.assertIsNotNone(actual, msg="'1' impute condition failed")
         actual = actual[0] # unwrap
         if expected_impute_value == "__NOT__PROVIDED__":