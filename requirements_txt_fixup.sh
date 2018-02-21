0118b535b66da9720a628de021a07fb1c9bd777c
Joshua Tauberer <jt@occams.info>
2018-02-21 12:42:59 -0500

    change URL for patched django-notifications-hq since the branch was renamed, see 4006f37561fcfc526753bc6b2b8412648b4d898a

--- requirements_txt_fixup.sh
+++ requirements_txt_fixup.sh
@@ -6,7 +6,7 @@ REQUIREMENTS_FILE=$1
 
 # Fixup - get django-notifications-hq from a Github repo that applies
 # a patch for Django 2.0 compatibility.
-python3 -c "import sys, re; print(re.sub('(?<=\n)django-notifications-hq==.*(\n\s+--hash.*)+', 'https://github.com/phamk/django-notifications/archive/django-2.0.zip \\\\\n    --hash=sha256:f46ef7b5702772adbd343f692eac4fc9712625cd6167c9bc927282ac98b66969\n    # see https://github.com/phamk/django-notifications/commit/652222d2b8b0ad9c23e61cf4880f85dc40d699d0', sys.stdin.read()), end='')" \
+python3 -c "import sys, re; print(re.sub('(?<=\n)django-notifications-hq==.*(\n\s+--hash.*)+', 'https://github.com/phamk/django-notifications/archive/django-2-compatibility.zip \\\\\n    --hash=sha256:4da98487dda2ffcdc108f5d30db4552055056cecedb7e08edab616de763927ed\n    # see https://github.com/phamk/django-notifications/commit/652222d2b8b0ad9c23e61cf4880f85dc40d699d0', sys.stdin.read()), end='')" \
 	< $REQUIREMENTS_FILE \
 	> $REQUIREMENTS_FILE.new
 mv $REQUIREMENTS_FILE.new $REQUIREMENTS_FILE
