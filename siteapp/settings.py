GovReady/govready-q

commit ca514b51bc38900e4072a9a4a18352d74978925a

add console logging when DEBUG is False
Django's default LOGGING setup removes some console logging when DEBUG = False. We still
get the runserver request logging, but we don't get unhandled exception stack traces.
Add back >=WARNING logging to the console.

This is helpful witH Docker deployments to see what is causing a 500 server error response.

siteapp/settings.py
@@ -212,14 +212,14 @@ if environment.get('memcached'):
 	SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 
 # Logging.
+# Django disables console logging when DEBUG is false but console logging
+# is handy, especially in simple Docker deployments. Unhandled exception
+# stack traces are logged to the console.
 from django.utils.log import DEFAULT_LOGGING
 LOGGING = DEFAULT_LOGGING
-LOGGING['loggers'].update({
-	'': {
-		'handlers': ['console'],
-		'level': 'INFO',
-	},
-})
+if not DEBUG:
+	LOGGING['handlers']['console']['filters'].remove('require_debug_true')
+	LOGGING['handlers']['console']['level'] = 'WARNING'
 if "syslog" in environment:
 	# install the rfc5424-logging-handler package
 	import socket

commit 71b675dbc5d88b55dc0414ca7a84c742352c2520

fix bug introduced by ca514b51bc38900e4072a9a4a18352d74978925a
The earlier commit changed the LOGGING setup. We had a root logger set up
so that when a syslog receiver is configured it picks up everything that
I removed. This puts it back just for the syslogger.

siteapp/settings.py
@@ -221,7 +221,8 @@ if not DEBUG:
 	LOGGING['handlers']['console']['filters'].remove('require_debug_true')
 	LOGGING['handlers']['console']['level'] = 'WARNING'
 if "syslog" in environment:
-	# install the rfc5424-logging-handler package
+	# install the rfc5424-logging-handler package. Add a top-level
+	# logger that sends everything to the syslog receiver.
 	import socket
 	hostname, port = environment["syslog"].split(":", 1)
 	LOGGING['formatters'].update({
@@ -240,7 +241,10 @@ if "syslog" in environment:
 			'hostname': environment['host'] + "@" + socket.gethostname()
 		}
 	})
-	LOGGING['loggers']['']['handlers'].append('syslog')
+	LOGGING['loggers'][''] = {
+		'handlers': ['syslog'],
+		'level': 'INFO',
+	}
 
 # Settings that have normal values based on the primary app
 # (the app this file resides in).