GovReady/govready-q

commit 88f396e2da8e26db958a1bfaae69fb465c9c5a71

add a new {{q.title}} field to get the title of the sub-task that answers the question

guidedmodules/module_logic.py
@@ -1041,6 +1041,8 @@ class TemplateContext(Mapping):
         # The context also provides the project and organization that the Task belongs to,
         # and other task attributes, assuming the keys are not overridden by question IDs.
         if self.module_answers and self.module_answers.task:
+            if item == "title":
+                return self.module_answers.task.title
             if item == "task_link":
                 return self.module_answers.task.get_absolute_url()
             if item == "project":
@@ -1098,7 +1100,7 @@ class TemplateContext(Mapping):
         # special values
         if self.module_answers and self.module_answers.task:
             # Attributes that are only available if there is a task.
-            for attribute in ("task_link", "project", "organization"):
+            for attribute in ("title", "task_link", "project", "organization"):
                 if attribute not in seen_keys:
                     yield attribute
 

commit 46c170112582f3a65890109a198b4eb713e2bdf8

rendering instance-name Task titles was broken by 88f396e2da8e26db958a1bfaae69fb465c9c5a71 which added {{q.title}}
At the root template context, '{{title}}' should not be offered as a context variable when computing the Task's own title since it will be
immediately evaluated and lead to infinite recursion

guidedmodules/module_logic.py
@@ -1041,7 +1041,7 @@ class TemplateContext(Mapping):
         # The context also provides the project and organization that the Task belongs to,
         # and other task attributes, assuming the keys are not overridden by question IDs.
         if self.module_answers and self.module_answers.task:
-            if item == "title":
+            if item == "title" and (not self.is_computing_title or not self.root):
                 return self.module_answers.task.title
             if item == "task_link":
                 return self.module_answers.task.get_absolute_url()
@@ -1100,7 +1100,11 @@ class TemplateContext(Mapping):
         # special values
         if self.module_answers and self.module_answers.task:
             # Attributes that are only available if there is a task.
-            for attribute in ("title", "task_link", "project", "organization"):
+            if not self.is_computing_title or not self.root:
+                # 'title' isn't available if we're in the process of
+                # computing it
+                yield "title"
+            for attribute in ("task_link", "project", "organization"):
                 if attribute not in seen_keys:
                     yield attribute
 