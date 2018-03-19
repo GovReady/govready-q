GovReady/govready-q

commit 4073d8c35e23c184aa2f07d9a40762d486ebfbae

add a help squad to Organizations and invite them into Discussions after the first comment on a Discussion

guidedmodules/models.py
@@ -1052,11 +1052,22 @@ class TaskAnswer(models.Model):
                 "notification_text": str(answer.answered_by) + " " + vp + "."
             })
 
+        # The invitation of the help squad.
+        if (self.extra or {}).get("invited-help-squad"):
+            history.append({
+                "type": "event",
+                "date": self.extra["invited-help-squad"],
+                "html": html.escape("Help squad invited to this discussion."),
+                "notification_text": "Help squad invited to this discussion.",
+            })
+
         # Sort.
         history.sort(key = lambda item : item["date"])
 
         # render events for easier client-side processing
+        from django.utils.dateparse import parse_datetime
         for item in history:
+            if isinstance(item["date"], str): item["date"] = parse_datetime(item["date"])
             item["date_relative"] = reldate(item["date"], timezone.now()) + " ago"
             item["date_posix"] = item["date"].timestamp()
             del item["date"] # not JSON serializable
@@ -1235,6 +1246,32 @@ class TaskAnswer(models.Model):
                 for term in organization.extra.get("vocabulary", [])
             ]
         }
+
+    # required to attach a Discussion to it
+    def on_discussion_comment(self, comment):
+        # A comment was left. If we haven't already, invite all organization
+        # help crew members to this discussion. See siteapp.views.send_invitation
+        # for how to construct Invitations.
+        from siteapp.models import Invitation
+        self.extra = self.extra or { } # ensure initialized
+        if not self.extra.get("invited-help-squad"):
+            anyone_invited = False
+            for user in self.task.project.organization.help_squad.all():
+                if user in self.get_notification_watchers(): continue # no need to invite
+                inv = Invitation.objects.create(
+                    organization=self.task.project.organization,
+                    from_user=comment.user,
+                    from_project=self.task.project,
+                    target=comment.discussion,
+                    target_info={ "what": "invite-guest" },
+                    to_user=user,
+                    text="The organization's help squad is being automatically invited to help with the following comment:\n\n" + comment.text,
+                )
+                inv.send()
+                anyone_invited = True
+            if anyone_invited:
+                self.extra["invited-help-squad"] = timezone.now()
+                self.save()
 
 
 class TaskAnswerHistory(models.Model):

discussion/models.py
@@ -287,6 +287,10 @@ class Comment(models.Model):
             description=self.text,
             comment_id=self.id)
 
+        # Let the owner object of the discussion know that a comment was left.
+        if hasattr(self.discussion.attached_to, 'on_discussion_comment'):
+            self.discussion.attached_to.on_discussion_comment(self)
+
 
     def push_history(self, field):
         if not isinstance(self.extra, dict):

siteapp/models.py
@@ -156,6 +156,8 @@ class Organization(models.Model):
     name = models.CharField(max_length=256, help_text="The display name of the Organization.")
     subdomain = models.CharField(max_length=256, unique=True, help_text="The subdomain of the host site that this Organization's site is served at.", validators=[RegexValidator(regex=subdomain_regex)])
 
+    help_squad = models.ManyToManyField(User, blank=True, help_text="Users who are invited to all new discussions in this Organization.")
+
     created = models.DateTimeField(auto_now_add=True, db_index=True)
     updated = models.DateTimeField(auto_now=True, db_index=True)
     extra = JSONField(default={}, blank=True, help_text="Additional information stored with this object.")

siteapp/migrations/0020_organization_help_squad.py
@@ -1,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Generated by Django 1.10.7 on 2017-08-31 20:15
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('siteapp', '0019_user_api_key_wo'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='organization',
+            name='help_squad',
+            field=models.ManyToManyField(blank=True, help_text='Users who are invited to all new discussions in this Organization.', to=settings.AUTH_USER_MODEL),
+        ),
+    ]

commit 580a98dbc411a3db9292df92004591ae7a0d3fca

question pages crashed if the question had been answered and the help squad had been invited
A call to sort() crashed with 'unorderable types: str() < datetime.datetime()' just when a
question has had activity (i.e. was answered), which used a datetime, and when the help squad
had been invited, whose activity entry used a string. If a question didn't have both, the
datatypes were consistent and sortable. Only when both were present was there a problem.

The help squad invite date had to be parsed earlier, before sort().

caused by 4073d8c35e23c184aa2f07d9a40762d486ebfbae

guidedmodules/models.py
@@ -1277,10 +1277,11 @@ class TaskAnswer(models.Model):
             })
 
         # The invitation of the help squad.
+        from django.utils.dateparse import parse_datetime
         if (self.extra or {}).get("invited-help-squad"):
             history.append({
                 "type": "event",
-                "date": self.extra["invited-help-squad"],
+                "date": parse_datetime(self.extra["invited-help-squad"]),
                 "html": html.escape("Help squad invited to this discussion."),
                 "notification_text": "Help squad invited to this discussion.",
             })
@@ -1289,9 +1290,7 @@ class TaskAnswer(models.Model):
         history.sort(key = lambda item : item["date"])
 
         # render events for easier client-side processing
-        from django.utils.dateparse import parse_datetime
         for item in history:
-            if isinstance(item["date"], str): item["date"] = parse_datetime(item["date"])
             item["date_relative"] = reldate(item["date"], timezone.now()) + " ago"
             item["date_posix"] = item["date"].timestamp()
             del item["date"] # not JSON serializable