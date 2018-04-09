GovReady/govready-q

commit c9389b9ca446dd03d0bcc4da6e4bef5988062e63

docker: run the container as a non-root user
* Create a user and group each named "application".
* Give this user permission to write to our 'local' directory where we store application settings and the (non-persistent) Sqlite database.
* Move the 'ready' file from the application source directory, which is no longer writable at run-time, to /tmp. It's written at the end of container startup to flag to our docker_container_run.sh script that Q is about ready to be visited, so updated the path there too.

Dockerfile
@@ -68,5 +68,18 @@ COPY deployment/docker/first_run.sh .
 # the container is launched with --mount.
 RUN mkdir -p /mnt/apps
 
+# Create a non-root user and group for the application to run as to guard against
+# run-time modification of the system and application.
+RUN groupadd application && \
+    useradd -g application -d /tmp/home -s /sbin/nologin -c "application process" application && \
+    chown -R application:application /tmp/home
+
+# Give the non-root user access to scratch space.
+RUN mkdir -p local
+RUN chown -R application:application local
+
+# Run the container's process zero as this user.
+USER application
+
 # Set the startup script.
 CMD [ "bash", "dockerfile_exec.sh" ]

deployment/docker/docker_container_run.sh
@@ -253,7 +253,7 @@ echo
 # writes out a 'ready' file once migrations are finished,
 # and then it's probably another second before the Django
 # server is listening.
-while ! docker container exec ${CONTAINER_ID} test -f ready; do
+while ! docker container exec ${CONTAINER_ID} test -f /tmp/govready-q-is-ready; do
   echo "Waiting for GovReady-Q to come online..."
   sleep 3
 done

deployment/docker/dockerfile_exec.sh
@@ -3,11 +3,6 @@
 
 set -euf -o pipefail # abort script on error
 
-# Generate a local/environment.json file. Use the jq
-# tool to ensure that we produce valid JSON from
-# the environment variables.
-mkdir -p local
-
 # What's the address (and port, if not 80) that end users
 # will access the site at? If the HOST and PORT environment
 # variables are set (and PORT is not 80), take the values
@@ -53,7 +48,7 @@ fi
 
 # Write a file that indicates to the host that Q
 # is now fully configured.
-echo "done" > ready
+echo "done" > /tmp/govready-q-is-ready
 echo "GovReady-Q is fully up and running."
 
 # Start the server. The port is fixed --- see docker_container_run.sh.

commit 5ece825c44b1bb6253d2847e53ac84f6959c524b

docker: first_run script was broken by c9389b9ca446dd03d0bcc4da6e4bef5988062e63
* The path to the 'ready' flag file changed to /tmp.
* Also simplify the container location of the script to run without needing to type './' in docker exec.

Dockerfile
@@ -61,7 +61,7 @@ RUN python3.6 manage.py collectstatic --noinput
 
 # Add container startup scripts.
 COPY deployment/docker/dockerfile_exec.sh .
-COPY deployment/docker/first_run.sh .
+COPY deployment/docker/first_run.sh /usr/local/bin/first_run
 
 # This directory must be present for the AppSource created by our
 # first_run script. The directory only has something in it if

deployment/docker/first_run.sh
@@ -4,7 +4,7 @@ set -euf -o pipefail # abort script on error
 
 # Check that the database is ready. The docker exec command
 # writes out a 'ready' file once migrations are finished.
-while [ ! -f ready ]; do
+while [ ! -f /tmp/govready-q-is-ready ]; do
 	echo "Waiting for the database to finish initializing..."
 	sleep 3
 done