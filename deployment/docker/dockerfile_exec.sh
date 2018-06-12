7bcc7f40ce2d8b53cc9363a975705d511a553565
Joshua Tauberer <jt@occams.info>
2018-06-12 10:33:33 -0400

    docker: echo filesystem information at container start so one can see read-only root file system and mount point configurations in the logs

--- deployment/docker/dockerfile_exec.sh
+++ deployment/docker/dockerfile_exec.sh
@@ -8,6 +8,14 @@ set -euf -o pipefail # abort script on error
 echo "This is GovReady-Q."
 cat VERSION
 
+# Show filesystem information because the root filesystem might be
+# read-only and other paths might be mounted with tmpfs and that's
+# helpful to know for debugging.
+echo
+echo Filesystem information:
+cat /proc/mounts | egrep -v "^proc|^cgroup| /proc| /dev| /sys"
+echo
+
 # Check that we're running as the 'application' user. Our Dockerfile
 # specifies to run containers as that user. But cluster environments
 # can override the start user and might do so to enforce running as
