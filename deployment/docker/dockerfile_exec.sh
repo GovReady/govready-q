GovReady/govready-q

commit d484b134bbf7220825eb0553c8af9c3a8853ecfe

docker: add ORGANIZATION_PARENT_DOMAIN, SYSLOG, ADMINS, SECRET_KEY, MAILGUN_API_KEY environment variables, document all of the variables, and make some environment settings optional

deployment/docker/dockerfile_exec.sh
@@ -10,31 +10,54 @@ set -euf -o pipefail # abort script on error
 ADDRESS="${HOST-localhost}:${PORT-8080}"
 ADDRESS=$(echo $ADDRESS | sed s/:80$//; )
 
-# Create a local/environment.json file. Use jq on some of
-# the inputs to guarantee valid JSON encoding of strings.
+# Create a local/environment.json file. Use jq to
+# guarantee valid JSON encoding of strings.
 cat > local/environment.json << EOF;
 { 
 	"debug": ${DEBUG-false},
 	"host": $(echo ${ADDRESS} | jq -R .),
 	"https": ${HTTPS-false},
-	"single-organization": "main",
+	"secret-key": $(echo ${SECRET_KEY-} | jq -R .),
+	"syslog": $(echo ${SYSLOG-} | jq -R .),
+	"admins": ${ADMINS-[]},
 	"static": "static_root",
 	"db": $(echo ${DBURL-} | jq -R .)
 }
 EOF
 
-echo "Starting GovReady-Q at ${ADDRESS} with HTTPS ${HTTPS-false}."
+function set_env_setting {
+	# set_env_setting keypath value
+	cat local/environment.json \
+	| jq ".[\"$1\"] = $(echo $2 | jq -R .)" \
+	> /tmp/new-environment.json
+	cat /tmp/new-environment.json > local/environment.json
+	rm -f /tmp/new-environment.json
+}
+
+# Add Q settings.
+if [ -z "${ORGANIZATION_PARENT_DOMAIN-}" ]; then
+	# Not multi-tenant with "main" as the subdomain of the
+	# default organization.
+	set_env_setting single-organization main
+else
+	# Multi-tenant.
+	set_env_setting organization-parent-domain "$ORGANIZATION_PARENT_DOMAIN"
+fi
 
 # Add email parameters.
 if [ ! -z "${EMAIL_HOST-}" ]; then
-	cat local/environment.json \
-	| jq ".email.host = $(echo ${EMAIL_HOST} | jq -R .)" \
-	| jq ".email.port = $(echo ${EMAIL_PORT} | jq -R .)" \
-	| jq ".email.user = $(echo ${EMAIL_USER} | jq -R .)" \
-	| jq ".email.pw = $(echo ${EMAIL_PW} | jq -R .)" \
-	| jq ".email.domain = $(echo ${EMAIL_DOMAIN} | jq -R .)" \
-	> local/environment.json
+	set_env_setting email.host "$EMAIL_HOST"
+	set_env_setting email.port "$EMAIL_PORT"
+	set_env_setting email.user "$EMAIL_USER"
+	set_env_setting email.pw "$EMAIL_PW"
+	set_env_setting email.domain "$EMAIL_DOMAIN"
 fi
+if [ ! -z "${MAILGUN_API_KEY-}" ]; then
+	set_env_setting mailgun_api_key "$MAILGUN_API_KEY"
+fi
+
+# Write out the settings that indicate where we think the site is running at.
+echo "Starting GovReady-Q at ${ADDRESS} with HTTPS ${HTTPS-false}."
 
 # Initialize the database.
 python3.6 manage.py migrate

siteapp/settings.py
@@ -44,8 +44,8 @@ else:
 SECRET_KEY = environment.get("secret-key") or make_secret_key()
 
 # The DEBUG flag must be set in the environment.
-DEBUG = environment["debug"]
-ADMINS = environment.get("admins", [])
+DEBUG = bool(environment.get("debug"))
+ADMINS = environment.get("admins") or []
 
 # Set ALLOWED_HOSTS from the host environment. If it has a port, strip it.
 # The port is used in SITE_ROOT_URL must must be removed from ALLOWED_HOSTS.
@@ -220,7 +220,7 @@ LOGGING = DEFAULT_LOGGING
 if not DEBUG:
 	LOGGING['handlers']['console']['filters'].remove('require_debug_true')
 	LOGGING['handlers']['console']['level'] = 'WARNING'
-if "syslog" in environment:
+if environment.get("syslog"):
 	# install the rfc5424-logging-handler package. Add a top-level
 	# logger that sends everything to the syslog receiver.
 	import socket

commit 648d821462a8f03d27ad66c38cecfe2a9579fe54

fix email settings in Docker
d484b134 broke the email environment variables because the ["..."]
quoting that was added, which is necessary to pass to the 'jq' tool JSON
keys that have dashes, prevented the "." in the email field keys from being
interpreted not as a literal "." but as targeting a sub-key, because we
store the settings as `"email": { "host": ... }`, not as `"email.host": ...`.

deployment/docker/dockerfile_exec.sh
@@ -28,7 +28,7 @@ EOF
 function set_env_setting {
 	# set_env_setting keypath value
 	cat local/environment.json \
-	| jq ".[\"$1\"] = $(echo $2 | jq -R .)" \
+	| jq ".$1 = $(echo $2 | jq -R .)" \
 	> /tmp/new-environment.json
 	cat /tmp/new-environment.json > local/environment.json
 	rm -f /tmp/new-environment.json
@@ -38,10 +38,10 @@ function set_env_setting {
 if [ -z "${ORGANIZATION_PARENT_DOMAIN-}" ]; then
 	# Not multi-tenant with "main" as the subdomain of the
 	# default organization.
-	set_env_setting single-organization main
+	set_env_setting '["single-organization"]' main
 else
 	# Multi-tenant.
-	set_env_setting organization-parent-domain "$ORGANIZATION_PARENT_DOMAIN"
+	set_env_setting '["organization-parent-domain"]' "$ORGANIZATION_PARENT_DOMAIN"
 fi
 
 # Add email parameters.