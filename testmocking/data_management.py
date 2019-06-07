b0290a01500c02ebd7efc1a813b9846aaf7ae457
Ethan Kaminski <ethan.kaminski@gmail.com>
2019-06-07 11:03:48 +0000

    Improve test User creation speed, support larger sets of test data

--- testmocking/data_management.py
+++ testmocking/data_management.py
@@ -23,9 +23,9 @@ def get_name(count, separator=' '):
     wordlist = get_wordlist()
     return separator.join([name.title() for name in sample(wordlist, count)])
 
-def create_user(username=None, password=None):
+def create_user(username=None, password=None, pw_hash=None):
     if username == None:
-        username = get_name(2, '_').lower()
+        username = get_name(3, '_').lower()
     if password == None:
         password = get_random_string(16)
 
@@ -34,8 +34,11 @@ def create_user(username=None, password=None):
             username=username,
             email=("testuser_%s@govready.com" % username),
         )
-        u.clear_password = password
-        u.set_password(u.clear_password)
+        if pw_hash:
+            u.password = pw_hash
+        else:
+            u.clear_password = password
+            u.set_password(u.clear_password)
         u.save()
         file.write(str(u.id))
         file.write("\n")
@@ -43,8 +46,8 @@ def create_user(username=None, password=None):
 
 def create_organization(name=None, admin=None, help_squad=[], reviewers=[]):
     if name == None:
-        name = get_name(1).upper()
-    subdomain = name.lower()
+        name = get_name(2).upper()
+    subdomain = name.replace(' ', '-').lower()
 
     with open(_getpath(Organization), 'a+') as file:
         org = Organization.create(
--- testmocking/management/commands/populate.py
+++ testmocking/management/commands/populate.py
@@ -22,14 +22,21 @@ class Command(BaseCommand):
         
         parser.add_argument('--user-count', type=int, default=20)
         parser.add_argument('--org-count', type=int, default=5)
+        parser.add_argument('--print-iter', type=int, default=100)
 
     def handle(self, *args, **options):
         users = []
+        pw_hash = None
         for x in range(0, options['user_count']):
-            u = create_user(password=options['password'])
+            u = create_user(password=options['password'], pw_hash=pw_hash)
+            pw_hash = u.password # MASSIVE performance optimization here
             users += [u]
+            if ((x+1) % options['print_iter'] == 0):
+                print("Created user #" + str(x+1))
         for x in range(0, options['org_count']):
             admin = sample(users, 1)[0]
-            org = create_organization(admin=admin)
+            help_squad = sample(users, 5)
+            reviewers = sample(users, 5)
+            org = create_organization(admin=admin, help_squad=help_squad, reviewers=reviewers)
             print("Admin: " + admin.username)
 
