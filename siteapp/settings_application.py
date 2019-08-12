e8cf946f5f0390ca438ff46fbea1dc5a5ab07ead
Julie Kramer <thejuliekramer@gmail.com>
2019-08-12 14:24:25 -0400

    add hide registration admin flag (#695)
    * add hide registration admin flag
    
    * Include system_settings model Docker build

--- siteapp/settings_application.py
+++ siteapp/settings_application.py
@@ -14,6 +14,7 @@ INSTALLED_APPS += [
     'siteapp',
     'guidedmodules',
     'discussion',
+    'system_settings',
 
     'testmocking',
 ]
---
+++ system_settings/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SystemSettingsConfig(AppConfig):
+    name = 'system_settings'
