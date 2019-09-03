c809f8f09dba179fc9a490c44420a54f12041643
Julie Kramer <thejuliekramer@gmail.com>
2019-09-03 15:26:31 -0500

    print so execute does not fail

--- siteapp/management/commands/db_before_090.py
+++ siteapp/management/commands/db_before_090.py
@@ -11,17 +11,17 @@ class Command(BaseCommand):
     site_app_migration_exists = MigrationRecorder.Migration.objects.filter(app='siteapp', name='0025_auto_20190515_1455')
     guardian_migration_exists = MigrationRecorder.Migration.objects.filter(app='guardian', name='0001_initial')
     system_settings_exists = MigrationRecorder.Migration.objects.filter(app='system_settings', name='0002_auto_20190808_1947')
-
-    migration_to_090_occured = False
+    
+    DB_BEFORE_090 = False
 
     if site_app_migration_exists:
       print("site app migrations have been run")
-      migration_to_090_occured = True
+      DB_BEFORE_090 = True
     if guardian_migration_exists:
       print("guardian migrations have been run")
-      migration_to_090_occured = True
+      DB_BEFORE_090 = True
     if system_settings_exists:
       print("system_settings migrations have been run")
-      migration_to_090_occured = True
+      DB_BEFORE_090 = True
 
-    print(migration_to_090_occured)
+    print(DB_BEFORE_090)
