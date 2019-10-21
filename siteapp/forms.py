9d05a83d47aa9d2e3ed0e174deebe961924115a5
Dayton Williams <dayton.williams@govready.com>
2019-10-21 17:16:53 -0400

    Added labels for form fields. Host instance description added.

--- siteapp/forms.py
+++ siteapp/forms.py
@@ -29,12 +29,22 @@ class SystemInstanceForm(ModelForm):
     class Meta:
         model = SystemInstance
         fields = ['name', 'sdlc_stage']
+        labels = {
+            'name': ('Name'),
+            'sdlc_stage': ('Software Development Life Cycle (SDLC) Stage'),
+        }
 
 class HostInstanceForm(ModelForm):
 
     class Meta:
         model = HostInstance
         fields = ['name', 'host_type', 'os', 'system_instance']
+        labels = {
+            'name': ('Name'),
+            'host_type': ('Host Type'),
+            'os': ('Operating System'),
+            'system_instance': ('System Instance'),
+        }
 
 
 class PortfolioSignupForm(ModelForm):
