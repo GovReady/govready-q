303bf4dee6041ad0b60d07931bc31adbd9f2dab4
Greg Elin <greg.elin@govready.com>
2019-10-20 23:10:02 -0400

    Improve admin display of itsystem models

--- itsystems/admin.py
+++ itsystems/admin.py
@@ -3,7 +3,15 @@ from django.contrib import admin
 from .models import SystemInstance, HostInstance, AgentService, Agent
 
 
-admin.site.register(SystemInstance)
-admin.site.register(HostInstance)
+class SystemInstanceAdmin(admin.ModelAdmin):
+    ordering = ('name', 'sdlc_stage')
+    list_display = ('name', 'sdlc_stage', 'id')
+
+class HostInstanceAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+    list_display = ('name', 'system_instance', 'host_type', 'os')
+
+admin.site.register(SystemInstance, SystemInstanceAdmin)
+admin.site.register(HostInstance, HostInstanceAdmin)
 admin.site.register(AgentService)
 admin.site.register(Agent)
--- itsystems/models.py
+++ itsystems/models.py
@@ -9,6 +9,10 @@ class SystemInstance(models.Model):
     name = models.CharField(max_length=255, help_text="The name of this IT System Instance.", unique=True)
     sdlc_stage = models.CharField(max_length=24, null=True, blank=True, help_text="The stage of the System Development Life Cycle System is in.")
 
+    def __str__(self):
+        # For the admin, notification strings
+        return self.name
+
     def get_absolute_url(self):
         return "/itsystems/%s/" % (self.id)
 
@@ -18,12 +22,24 @@ class HostInstance(models.Model):
     os = models.CharField(max_length=155, null=True, blank=True, help_text="The Operating System running on the Host Instance.")
     system_instance = models.ForeignKey(SystemInstance, blank=True, null=True, related_name="system_instance", on_delete=models.SET_NULL, help_text="The SystemInstance to which this HostInstance belongs.")
 
+    def __str__(self):
+        # For the admin, notification strings
+        return self.name
+
 class AgentService(models.Model):
     name = models.CharField(max_length=255, help_text="The name of this endpoint/host Agent Service .", unique=True)
     api_user = models.CharField(max_length=255, null=True, blank=True, help_text="The user/login identify for accessing Agent Service's API.")
     api_pw = models.CharField(max_length=255, null=True, blank=True, help_text="The user/login password for accessing Agent Service's API.")
 
+    def __str__(self):
+        # For the admin, notification strings
+        return self.name
+
 class Agent(models.Model):
     agent_id = models.CharField(max_length=24, help_text="The unique identifier of an installed Agent on a Host Instance.")
     agent_service = models.ForeignKey(AgentService, null=False, blank=False, related_name="agent", on_delete="CASCADE", help_text="The AgentService to which this Agent belonts.")
     host_instance = models.ForeignKey(HostInstance, null=True, blank=True, related_name="agent", on_delete="models.SET_NULL", help_text="The HostInstance on which the Agent is installed and monitoring.")
+
+    def __str__(self):
+        # For the admin, notification strings
+        return self.agent_id
