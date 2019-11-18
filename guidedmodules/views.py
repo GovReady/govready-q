56c097c06b9dc5705861b8185be54025fe01499a
Peter Kaminski <kaminski@istori.com>
2019-11-18 12:32:24 -0800

    Add back check for 'questions.get(mq.id)'
    In test_invitations(), questions.get(mq.id) was None (I think when inviting me3).  This caused a Server Error 500, which caused the page title to be blank.

--- guidedmodules/views.py
+++ guidedmodules/views.py
@@ -652,7 +652,7 @@ def show_question(request, task, answered, context, q):
 
         # if questions.get(mq.id) and 'task' in questions[mq.id]:
         # print("** questions.get(mq.id)", questions.get(mq.id)) # Debug
-        if 'task' in questions[mq.id]:
+        if questions.get(mq.id) and 'task' in questions[mq.id]:
             # print("\nquestions[mq.id]['task'].id", questions[mq.id]['task'].id, questions[mq.id]['task']) # Debug
             task_link = "/tasks/{}/{}".format(questions[mq.id]['task'].id, "start")
             task_id = questions[mq.id]['task'].id
