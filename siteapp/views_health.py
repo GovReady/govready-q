f70edecbeac3f973fedd6411c0fe629b70143fa8
Peter Kaminski <kaminski@istori.com>
2019-11-20 10:08:39 -0800

    switch from os.popen to subprocess and add #nosec, for bandit

--- siteapp/views_health.py
+++ siteapp/views_health.py
@@ -1,18 +1,18 @@
-import os
+import subprocess #nosec
 
 from django.http import HttpResponse
 
 def check_system(request):
-    output = os.popen("./check-system.sh").read()
+    output = subprocess.check_output(["./check-system.sh"]).decode("utf-8")
     html = "<html><body><pre>{}</pre></body></html>".format(output)
     return HttpResponse(html)
 
 def check_vendor_resources(request):
-    output = os.popen("./check-vendor-resources.sh").read()
+    output = subprocess.check_output(["./check-vendor-resources.sh"]).decode("utf-8")
     html = "<html><body><pre>{}</pre></body></html>".format(output)
     return HttpResponse(html)
 
 def list_vendor_resources(request):
-    output = os.popen("./list-vendor-resources.sh").read()
+    output = subprocess.check_output(["./list-vendor-resources.sh"]).decode("utf-8")
     html = "<html><body><pre>{}</pre></body></html>".format(output)
     return HttpResponse(html)
