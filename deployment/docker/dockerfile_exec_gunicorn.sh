1613a5cf14075d3a06aebc08ba2b3bfad1376287
Peter Kaminski <peter.kaminski@govready.com>
2020-05-18 10:21:10 -0700

    add note about workers count

--- deployment/docker/dockerfile_exec_gunicorn.sh
+++ deployment/docker/dockerfile_exec_gunicorn.sh
@@ -148,6 +148,9 @@ fi
 cat > /tmp/gunicorn.conf.py <<EOF;
 import multiprocessing
 bind = '0.0.0.0:8000'
+# workers = multiprocessing.cpu_count() * 2 + 1 # recommended for high-traffic sites
+# set workers to 1 for now, because the session key won't be shared when it is auto-generated,
+# which causes the login session for users to drop as soon as they hit a different worker
 workers = 1
 worker_class = 'gevent'
 keepalive = 10
