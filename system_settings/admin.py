e2ff29505a400390d131098492a0fad27cb6bd93
Greg Elin <greg.elin@govready.com>
2020-08-06 08:07:13 -0500

    Create models for custom support page

--- system_settings/admin.py
+++ system_settings/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import SystemSettings
+from .models import SystemSettings, Support
 
 class SystemSettingsAdmin(admin.ModelAdmin):
   list_display = ('setting', 'active')
   fields = ('setting', 'active')
 
+class SupportAdmin(admin.ModelAdmin):
+  list_display = ('id', 'support_email',)
+  fields = ('support_text', 'support_email', 'support_phone')
+
 admin.site.register(SystemSettings, SystemSettingsAdmin)
+admin.site.register(Support, SupportAdmin)
--- system_settings/models.py
+++ system_settings/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class SystemSettings(models.Model):
+  """Model for various system settings for install of GovReady"""
+
   setting = models.CharField(max_length=200, unique=True)
   active = models.BooleanField(default=False)
 
@@ -19,3 +21,13 @@ class SystemSettings(models.Model):
   def enable_experimental_oscal(cls):
     return cls.objects.get(setting="enable_experimental_opencontrol").active
 
+class Support(models.Model):
+  """Model for support infomation for support page for install of GovReady"""
+
+  support_email = models.EmailField(max_length=254, unique=False, blank=True, null=True, help_text="Support email address")
+  support_phone = models.CharField(max_length=24, unique=False, blank=True, null=True, help_text="Support phone number")
+  support_text = models.TextField(unique=False, blank=True, null=True, help_text="Support desription at top of page")
+  support_url = models.URLField(max_length=200, unique=False, blank=True, null=True, help_text="Support url")
+
+  def __str__(self):
+    return "Support information"
