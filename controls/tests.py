888d639580555e955c26bd4a41fae326f468bb60
Greg Elin <greg.elin@govready.com>
2020-11-18 06:36:28 -0500

    [WIP] Adding control statement v prototype automated tests

--- controls/tests.py
+++ controls/tests.py
@@ -17,37 +17,17 @@ from unittest import skip
 
 from django.conf import settings
 from django.test import TestCase
-
+from django.utils.crypto import get_random_string
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from .oscal import Catalogs, Catalog
+from siteapp.models import (Organization, Portfolio, Project,
+                            ProjectMembership, User)
 from .models import *
 from siteapp.models import User
 
-# from controls.oscal import Catalogs, Catalog
-
-# ####### siteapp.test
-# import os
-# import os.path
-# import re
-# from unittest import skip
-
-# from django.conf import settings
-# from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-# from django.utils.crypto import get_random_string
-
-# from siteapp.models import (Organization, Portfolio, Project,
-#                             ProjectMembership, User)
-
-# ######guidedmodules.test
-# from django.test import TestCase
-# from django.conf import settings
-
-# from siteapp.models import Organization, Project, User
-# from guidedmodules.models import Module, Task, TaskAnswer
-# from guidedmodules.module_logic import *
-# from guidedmodules.app_loading import load_app_into_database
-
+# Import SeleniumTest and OrganizationSiteFunctionalTests from siteapp to keep things DRY
+from siteapp.tests import SeleniumTest, OrganizationSiteFunctionalTests
 
 def var_sleep(duration):
     '''
@@ -56,140 +36,6 @@ def var_sleep(duration):
     from time import sleep
     sleep(duration*2)
 
-class SeleniumTest(StaticLiveServerTestCase):
-    window_geometry = (1200, 1200)
-
-    @classmethod
-    def setUpClass(cls):
-        super(SeleniumTest, cls).setUpClass()
-
-        # Override the email backend so that we can capture sent emails.
-        from django.conf import settings
-        settings.EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
-
-        # Override ALLOWED_HOSTS, SITE_ROOT_URL, etc.
-        # because they may not be set or set properly in the local environment's
-        # non-test settings for the URL assigned by the LiveServerTestCase server.
-        settings.ALLOWED_HOSTS = ['localhost', 'testserver']
-        settings.SITE_ROOT_URL = cls.live_server_url
-
-        # In order for these tests to succeed when not connected to the
-        # Internet, disable email deliverability checks which query DNS.
-        settings.VALIDATE_EMAIL_DELIVERABILITY = False
-
-        ## Turn on DEBUG so we can see errors better.
-        #settings.DEBUG = True
-
-        # Start a headless browser.
-        import selenium.webdriver
-        from selenium.webdriver.chrome.options import Options as ChromeOptions
-        options = selenium.webdriver.ChromeOptions()
-        if os.path.exists("/usr/bin/chromium-browser"):
-            options.binary_location = "/usr/bin/chromium-browser"
-        options.add_argument("disable-infobars") # "Chrome is being controlled by automated test software."
-        if SeleniumTest.window_geometry == "maximized":
-            options.add_argument("start-maximized") # too small screens make clicking some things difficult
-        else:
-            options.add_argument("--window-size=" + ",".join(str(dim) for dim in SeleniumTest.window_geometry))
-        options.add_argument("--incognito")
-        cls.browser = selenium.webdriver.Chrome(chrome_options=options)
-        cls.browser.implicitly_wait(3) # seconds
-
-        # Clean up and quit tests if Q is in SSO mode
-        if getattr(settings, 'PROXY_HEADER_AUTHENTICATION_HEADERS', None):
-            print("Cannot run tests.")
-            print("Tests will not run when IAM Proxy enabled (e.g., when `local/environment.json` sets `trust-user-authentication-headers` parameter.)")
-            cls.browser.quit()
-            super(SeleniumTest, cls).tearDownClass()
-            exit()
-
-    @classmethod
-    def tearDownClass(cls):
-        # Terminate the selenium browser.
-        cls.browser.quit()
-
-        # Run superclass termination.
-        super(SeleniumTest, cls).tearDownClass()
-
-    def setUp(self):
-        # clear the browser's cookies before each test
-        self.browser.delete_all_cookies()
-
-    def navigateToPage(self, path):
-        self.browser.get(self.url(path))
-
-    def url(self, path):
-        # Construct a URL to the desired page. Use self.live_server_url
-        # (set by StaticLiveServerTestCase) to determine the scheme, hostname,
-        # and port the test server is running on. Add the path.
-        import urllib.parse
-        return urllib.parse.urljoin(self.live_server_url, path)
-
-    def clear_field(self, css_selector):
-        self.browser.find_element_by_css_selector(css_selector).clear()
-
-    def fill_field(self, css_selector, text):
-        self.browser.find_element_by_css_selector(css_selector).send_keys(text)
-
-    def clear_and_fill_field(self, css_selector, text):
-        self.clear_field(css_selector)
-        self.fill_field(css_selector, text)
-
-    def click_element_with_link_text(self, text):
-        elem = self.browser.find_elements_by_link_text(text)
-        elem[0].click()
-
-    def click_element_with_xpath(self, xpath):
-        elem = self.browser.find_elements_by_xpath(xpath)
-        elem[0].click()
-
-    def click_element(self, css_selector):
-        # ensure element is on screen or else it can't be clicked
-        # see https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
-        elem = self.browser.find_element_by_css_selector(css_selector)
-        self.browser.execute_script("arguments[0].scrollIntoView({ behavior: 'instant', block: 'nearest', inline: 'nearest' });", elem)
-        elem.click()
-
-    def select_option(self, css_selector, value):
-        from selenium.webdriver.support.select import Select
-        e = self.browser.find_element_by_css_selector(css_selector)
-        Select(e).select_by_value(value)
-
-    def select_option_by_visible_text(self, css_selector, text):
-        from selenium.webdriver.support.select import Select
-        e = self.browser.find_element_by_css_selector(css_selector)
-        Select(e).select_by_visible_text(text)
-
-    def _getNodeText(self, css_selector):
-        node_text = self.browser.find_element_by_css_selector(css_selector).text
-        node_text = re.sub(r"\s+", " ", node_text) # normalize whitespace
-        return node_text
-
-    def assertInNodeText(self, search_text, css_selector):
-        self.assertIn(search_text, self._getNodeText(css_selector))
-    def assertNotInNodeText(self, search_text, css_selector):
-        self.assertNotIn(search_text, self._getNodeText(css_selector))
-
-    def assertNodeNotVisible(self, css_selector):
-        from selenium.common.exceptions import NoSuchElementException
-        with self.assertRaises(NoSuchElementException):
-            self.browser.find_element_by_css_selector(css_selector)
-
-    def pop_email(self):
-        self.assertTrue(self.has_more_email())
-        import django.core.mail
-        return django.core.mail.outbox.pop(0)
-
-    def has_more_email(self):
-        import django.core.mail
-        # The outbox attribute doesn't exist until the backend
-        # instance is initialized when the first message is sent.
-        outbox = getattr(django.core.mail, 'outbox', [])
-        return len(outbox) > 0
-
-#####################################################################
-
-# Control Tests
 
 class SampleTest(TestCase):
     ## Simply dummy test ##
@@ -209,7 +55,29 @@ class Oscal80053Tests(TestCase):
 
 #####################################################################
 
-class ControlUITests(SeleniumTest):
+class GeneralTests(OrganizationSiteFunctionalTests):
+
+    def test_homepage(self):
+        self.browser.get(self.url("/"))
+        self.assertRegex(self.browser.title, "Welcome to Compliance Automation")
+
+    def test_login(self):
+        # Test that a wrong password doesn't log us in.
+        self._login(password=get_random_string(4))
+        self.assertInNodeText("The username and/or password you specified are not correct.", "form#login_form .alert-danger")
+
+        # Test that a wrong username doesn't log us in.
+        self._login(username="notme")
+        self.assertInNodeText("The username and/or password you specified are not correct.", "form#login_form .alert-danger")
+
+        # Log in as a new user, log out, then log in a second time.
+        # We should only get the account settings questions on the
+        # first login.
+        self._login()
+        self.browser.get(self.url("/accounts/logout/"))
+        self._login()
+
+class ControlUITests(OrganizationSiteFunctionalTests):
     def test_homepage(self):
         self.browser.get(self.url("/controls/"))
 
@@ -229,10 +97,10 @@ class ControlUITests(SeleniumTest):
     #     self.assertInNodeText("XX-2", "#control-heading")
     #     self.assertInNodeText("The control XX-2 was not found in the control catalog.", "#control-message")
 
-# class ControlUIControlEditorTests(SeleniumTest):
-#     def test_homepage(self):
-#         self.browser.get(self.url("/controls/editor"))
-#         self.assertInNodeText("Test works", "p")
+class ControlUIControlEditorTests(OrganizationSiteFunctionalTests):
+    def test_homepage(self):
+        self.browser.get(self.url("/controls/editor"))
+        self.assertInNodeText("Test works", "p")
 
     # def test_control_lookup(self):
     #     self.browser.get(self.url("/controls/800-53/AU-2/"))
@@ -271,6 +139,28 @@ class StatementUnitTests(TestCase):
         smt2 = Statement.objects.get(pk=smt.id)
         self.assertEqual(smt.sid, "au.3")
         self.assertEqual(smt.status, "Partially Implemented")
+
+    def test_control_implementation_vs_prototype(self):
+        # Detection of difference in statement
+        # Create a smt
+        smt = Statement.objects.create(
+            sid = "au.3",
+            sid_class = "NIST_SP-800-53_rev4",
+            body = "This is a test statement.",
+            statement_type = "control_implementation",
+            status = "Implemented"
+        )
+        smt.save()
+        # Create statement prototype
+        smt.create_prototype()
+        self.assertEqual(smt.body, smt.prototype.body)
+        self.assertNotEqual(smt.id, smt.prototype.id)
+        self.assertTrue(smt.prototype_synched)
+        # Change statement compared to prototype
+        smt.prototype.body = smt.prototype.body + "\nModified statememt"
+        smt.prototype.save()
+        self.assertFalse(smt.prototype_synched)
+        self.assertEqual(smt.diff_prototype_main, [(0, 'This is a test statement.'), (-1, '\nModified statememt')])
 
 class ElementUnitTests(TestCase):
     ## Simply dummy test ##
