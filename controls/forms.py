208fac5a310ae947b6c4eb3c2a12df683af38966
davidpofo <dampofo@umd.edu>
2020-12-14 09:28:08 -0500

    Update forms.py
    Provide the default value system_element for element_type.

--- controls/forms.py
+++ controls/forms.py
@@ -60,6 +60,8 @@ class ElementForm(ModelForm):
     class Meta:
         model = Element
         fields = ['name', 'full_name', 'description', 'element_type']
+        
+    element_type = forms.CharField(widget=forms.HiddenInput(), initial='system_element')
 
     def clean(self):
         """Extend clean to validate element name is not reused."""
