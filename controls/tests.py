78e3b183d8b631c9702d2e7281f3fb9e218b90d2
davidpofo <dampofo@umd.edu>
2021-01-12 15:46:46 -0500

    added tests for the import and export of project JSON

--- controls/tests.py
+++ controls/tests.py
@@ -896,3 +896,107 @@ class ControlTestHelper(object):
         statement.save()
 
         return import_record
+
+class ImportExportProjectTests(OrganizationSiteFunctionalTests):
+    """
+    Testing the whole import of project JSON which will form a new system, project, and set of components and their statements.
+    """
+
+    def test_new_project_json_import(self):
+        """
+        Testing the import of a new project through JSON
+        """
+
+        # login as the first user and create a new project
+        self._login()
+        self._new_project()
+
+        # Checks the number of projects and components before the import
+        self.assertEqual(Project.objects.all().count(), 3)
+        self.assertEqual(Element.objects.all().exclude(element_type='system').count(), 0)
+
+        ## Import a new project
+        # click import project button, opening the modal
+        self.click_element("#action-buttons\ action-row > div.btn-group > button:nth-child(4)")
+        ## select through the modal information needed and browse for the import needed
+        self.select_option_by_visible_text("#id_appsource_compapp", "project")
+        self.browser.find_element_by_xpath("(//option[@value='3'])[2]").click()
+        self.browser.find_element_by_id("id_importcheck").click()
+        #self.browser.find_element_by_id("id_file").click()
+        file_input = self.browser.find_element_by_css_selector("#id_file")
+
+        file_path = os.getcwd() + "/fixtures/test_project_import_data.json"
+        # convert filepath if necessary and send keys
+        self.filepath_conversion(file_input, file_path, "sendkeys")
+        self.browser.find_element_by_id("import_component_submit").click()
+
+        # Check the new number of projects, and validate that it's 1 more than the previous count.
+        self.assertEqual(Project.objects.all().count(), 4)
+        # Has the correct name?
+        self.assertEqual(Project.objects.get(id=4).title, "New Test Project")
+        # Components and their statements?
+        self.assertEqual(Element.objects.all().exclude(element_type='system').count(), 1)
+        self.assertEqual(Statement.objects.all().count(), 3)
+
+    def test_update_project_json_import(self):
+        """
+        Testing the update of a project through project JSON import and ingestion of components and their statements
+        """
+
+        # login as the first user and create a new project
+        self._login()
+        self._new_project()
+
+        # Checks the number of projects and components before the import
+        self.assertEqual(Project.objects.all().count(), 3)
+        self.assertEqual(Element.objects.all().exclude(element_type='system').count(), 0)
+
+        ## Update currentproject
+        # click import project button, opening the modal
+        self.click_element("#action-buttons\ action-row > div.btn-group > button:nth-child(4)")
+        ## select through the modal information needed and browse for the import needed
+        self.select_option_by_visible_text("#id_appsource_compapp", "project")
+        try:
+            self.select_option_by_visible_text("#id_appsource_version_id", "<project/simple_project(3)>")
+        except:
+            self.select_option_by_visible_text("#id_appsource_version_id", "<project/simple_project(9)>")
+
+
+        #self.browser.find_element_by_id("id_file").click()
+        file_input = self.browser.find_element_by_css_selector("#id_file")
+
+        file_path = os.getcwd() + "/fixtures/test_project_import_data.json"
+        # convert filepath if necessary and send keys
+        self.filepath_conversion(file_input, file_path, "sendkeys")
+        self.browser.find_element_by_id("import_component_submit").click()
+
+        # Check the new number of projects, and validate that it's the same
+        self.assertEqual(Project.objects.all().count(), 3)
+        # Has the updated name?
+        self.assertEqual(Project.objects.get(id=3).title, "New Test Project")
+
+        # Components and their statements?
+        self.assertEqual(Element.objects.all().exclude(element_type='system').count(), 1)
+        self.assertEqual(Statement.objects.all().count(), 3)
+
+    def test_project_json_export(self):
+        """
+        Testing the export of a project through JSON and ingestion of components and their statements
+        """
+
+        # login as the first user and create a new project
+        self._login()
+        self._new_project()
+
+        # export the only project we have so far
+        self.navigateToPage('/systems/3/export')
+
+        # Project title to search for in file names
+        project_title = "I_want_to_answer_some_questions_on_Q._3"
+        file_system = os.listdir(os.getcwd())
+        # Assert there is a file
+        for file_name in file_system:
+            if file_name ==project_title:
+                project_title = file_name
+
+        self.assertIn(file_name, file_system)
