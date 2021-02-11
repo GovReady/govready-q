9b15736259f255d9c115484c77b08c3bceda48df
davidpofo <dampofo@umd.edu>
2021-02-11 11:22:46 -0500

    wait for the tab-signin

--- siteapp/tests.py
+++ siteapp/tests.py
@@ -377,7 +377,7 @@ class OrganizationSiteFunctionalTests(SeleniumTest):
         # with other credentials.
         self.browser.get(self.url("/"))
         self.assertRegex(self.browser.title, "Welcome to Compliance Automation")
-        self.click_element("li#tab-signin")
+        wait_for_sleep_after(lambda: self.click_element("li#tab-signin"))
         self.fill_field("#id_login", username or self.user.username)
         self.fill_field("#id_password", password or self.user.clear_password)
         self.click_element("form#login_form button[type=submit]")
