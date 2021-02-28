131eda7a7a636744906f4f9659990b357dfa7b47
Alexander Ward <alexanderward@gmail.com>
2021-02-28 12:33:24 -0600

    changed hash type to be more secure

--- siteapp/utils/uploads.py
+++ siteapp/utils/uploads.py
@@ -1,6 +1,6 @@
 def hash_file(file, block_size=65536):
     import hashlib
-    hasher = hashlib.md5()
+    hasher = hashlib.sha512()
     from functools import partial
     for buf in iter(partial(file.read, block_size), b''):
         hasher.update(buf)
