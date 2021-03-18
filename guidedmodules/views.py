3b271f294747762746ffb629021c6a196355d382
Greg Elin <greg.elin@govready.com>
2021-03-18 08:15:27 -0500

    Fix import of messages

--- guidedmodules/views.py
+++ guidedmodules/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404, HttpResponse, HttpResponseRedirect, HttpResponseForbidden, JsonResponse, HttpResponseNotAllowed
-from django.contrib.auth.decorators import login_required, permission_required, messages
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required, permission_required
 from django.conf import settings
 from django.utils import timezone
 from django.db import transaction
