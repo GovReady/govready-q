75ee35af632715fe0a7d67dc42abec6d4da7f2ef
Greg Elin <greg.elin@govready.com>
2021-03-25 06:15:40 -0500

    Create a simple SAR server to generate test data
    Create a simple System Assessment Result web server
    generating JSON data object listing assessment summary
    results that can be added to a system's assessment.
    
    This provides a synthentic provider of SAR results data from
    which we can test the UI for consuming SAR data.

---
+++ tools/simple_sar_server/sar.py
@@ -0,0 +1,58 @@
+########################################################
+#
+# A simple Python webserver to generate random
+# assessment results
+#
+# Usage:
+#   python sar.py
+#
+# Accessing:
+#   curl localhost:8888
+#
+#######################################################
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+import random
+import uuid
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def mk_sar(self):
+
+        sar = {
+            "id": "{random.randint(20431, 34554)}",
+            "name": "oscap-scan",
+            "description": "SCAP scan Results",
+            "ip": f"10.10.0.{random.randint(2, 250)}",
+            "uuid": f"{str(uuid.uuid4())}",
+            "pass": random.randint(200, 300),
+            "fail": random.randint(0, 20),
+            "error": random.randint(0, 4)
+        }
+        print(sar)
+
+        return sar
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+
+        # Generate a random small number of endpoint assessments
+        sar_list = []
+        for endpoints in range(0,random.randint(1, 4)):
+            sar_list.append(self.mk_sar())
+        data = {"schema": "GovReadySimpleSAR",
+                "version": "0.1",
+                "sar": sar_list
+        }
+        # print(data)
+
+        # Send the JSON response
+        self.wfile.write(json.dumps(data, indent=4).encode('UTF-8'))
+
+
+httpd = HTTPServer(('localhost', 8888), SimpleHTTPRequestHandler)
+httpd.serve_forever()
