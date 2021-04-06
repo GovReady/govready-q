935a7078fc9dbcc7cbebc3ee3ead24062336ed53
Alexander Ward <alexander.ward1@gmail.com>
2021-04-06 15:55:34 -0500

    removed ssh certs and added generation instead

--- deployment/local/docker-entrypoint.sh
+++ deployment/local/docker-entrypoint.sh
@@ -1,6 +1,14 @@
 mkdir -p local
 
 echo "[ + ] Setting up SSH for remote Interpreter use"
+if [[ ! -d /usr/src/app/deployment/local/ssh ]]; then
+    echo "Configuring SSH Daemon"
+    mkdir /usr/src/app/deployment/local/ssh
+    sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+    ssh-keygen -A
+    chmod -R 400 /etc/ssh
+    cp /etc/ssh /usr/src/app/deployment/local -r
+fi
 /usr/sbin/sshd
 export -p > deployment/local/remote_interpreter/env_var.sh
 
