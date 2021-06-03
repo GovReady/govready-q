a080b7456782c8ce8759f1572f63d48f3c3df705
Greg Elin <greg.elin@govready.com>
2021-06-02 19:40:33 -0500

    Polish SSP control CSV export form

--- guidedmodules/forms.py
+++ guidedmodules/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class ExportCSVTemplateSSPForm(forms.Form):
 
-    info_system = forms.CharField(label='Column Header for `Program Name`', widget=forms.Textarea(attrs={"rows":1, "cols":3, 'style':'resize:none;', 'placeholder': 'Information System or Program Name (e.g. GovReady Project Name)'}))
-    control_id = forms.CharField(label='Column Header for `Control ID`', widget=forms.Textarea(attrs={"rows":1, "cols":3, 'style':'resize:none;', 'placeholder': 'Control Number (e.g. GovReady Control Ids)'}))
-    catalog = forms.CharField(label='Column Header for `Control Catalog`', widget=forms.Textarea(attrs={"rows":1, "cols":3, 'style':'resize:none;', 'placeholder': 'Control Set Version Number (e.g. GovReady Control Catalog)'}))
-    shared_imps = forms.CharField(label='Column Header for `Shared Implementation Details`', widget=forms.Textarea(attrs={"rows":1, "cols":3, 'style':'resize:none;', 'placeholder': 'Shared Implementation Details (e.g. GovReady full inherits from...)'}))
-    private_imps = forms.CharField(label='Column Header for `Control Implementation Statements`', widget=forms.Textarea(attrs={"rows":1, "cols":3, 'style':'resize:none;', 'placeholder': 'Private Implementation Details (e.g. GovReady does this...)'}))
+    info_system = forms.CharField(label='"Project Name" exported to column named: ', widget=forms.TextInput(attrs={'class': 'form-control', 'style':'resize:none;width:500px;', 'placeholder': 'Information System'}))
+    control_id = forms.CharField(label='"Control ID" exported to column named:', widget=forms.TextInput(attrs={'class': 'form-control', 'style':'resize:none;width:500px;', 'placeholder': 'Control ID'}))
+    catalog = forms.CharField(label='"Control Catalog" exported to column named:', widget=forms.TextInput(attrs={'class': 'form-control', 'style':'resize:none;width:500px;', 'placeholder': 'Control Set Version Number'}))
+    shared_imps = forms.CharField(label='"Implementation Statement (Library)" exported to column named:', widget=forms.TextInput(attrs={'class': 'form-control', 'style':'resize:none;width:500px;', 'placeholder': 'Shared Implementation Details'}))
+    private_imps = forms.CharField(label='"Implementation Statement (Local)" exported to column named:', widget=forms.TextInput(attrs={'class': 'form-control', 'style':'resize:none;width:500px;', 'placeholder': 'Private Implementation Details'}))
