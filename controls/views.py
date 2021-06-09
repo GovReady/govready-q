aa3099239be1e25a968ae61e96e91775aa4b76f2
davidpofo <dampofo@umd.edu>
2021-06-09 09:46:03 -0400

    displaying comparisons for x number of component statements against the prime component. Styling and abstracted out the comparison block into an included template

--- controls/views.py
+++ controls/views.py
@@ -440,32 +440,31 @@ def compare_components(request):
         element_list = list(Element.objects.filter(pk__in=compare_list).exclude(element_type='system').distinct())
         compare_prime, element_list = element_list[0], element_list[1:]# The first component selected will be compared against the rest
         compare_prime_smts = compare_prime.statements(StatementTypeEnum.CONTROL_IMPLEMENTATION_PROTOTYPE.value)
-        compare_secondary, element_list = element_list[0], element_list[1:]
     elif len(compare_list) <= 1:
         # add messages
         messages.add_message(request, messages.WARNING, f"Not enough components were selected to compare!")
         return HttpResponseRedirect("/controls/components")
     difference_tuples = []
     differences = []
-    # compare each component's statements to prime
-    cmt_smts = compare_secondary.statements(StatementTypeEnum.CONTROL_IMPLEMENTATION_PROTOTYPE.value)
-    if cmt_smts.exists():
-        # TODO: Need to create a tuple with smt id to return appropriate
-        for smt in cmt_smts:
-            smt_prime = compare_prime_smts.filter(sid=smt.sid).filter(pid=smt.pid).filter(sid_class=smt.sid_class).first()
-            # If it is not a statement found in both components then we just add styling
-            if smt_prime:
-                diff = diff_components_prettyHtml(smt_prime, smt)
-            else:
-                diff = f"<span><ins style='background:#e6ffe6;'>{smt.body}</ins><span>"
-            differences.append(diff)
-        difference_tuples.extend(zip(cmt_smts, differences))
-
+    for component in element_list:
+        differences = []
+        # compare each component's statements to prime
+        cmt_smts = component.statements(StatementTypeEnum.CONTROL_IMPLEMENTATION_PROTOTYPE.value)
+        if cmt_smts.exists():
+            # TODO: Need to create a tuple with smt id to return appropriate
+            for smt in cmt_smts:
+                smt_prime = compare_prime_smts.filter(sid=smt.sid).filter(pid=smt.pid).filter(sid_class=smt.sid_class).first()
+                # If it is not a statement found in both components then we just add styling
+                if smt_prime:
+                    diff = diff_components_prettyHtml(smt_prime, smt)
+                else:
+                    diff = f"<span><ins style='background:#e6ffe6;'>{smt.body}</ins><span>"
+                differences.append(diff)
+        difference_tuples.extend(zip([component.name] * len(cmt_smts), cmt_smts, differences))
     if request.method == 'POST':
         context = {
             "element_list": element_list,
             "compare_prime": compare_prime,
-            "compare_secondary": compare_secondary,
             "prime_smts": compare_prime_smts,
             "secondary_smts": cmt_smts,
             "differences": difference_tuples
