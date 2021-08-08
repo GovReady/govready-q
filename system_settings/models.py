72a8846ec049845c13e84d2c975b3d25cab880f2
Greg Elin <gregelin@govready.com>
2021-08-08 16:12:43 +0000

    Auto-start a particular project (#1640)
    * [WIP] Auto-start a particular project
    
    Enable the auto-starting of a particular project, e.g.,
    start a project without having to visit the compliance app
    store.
    
    Eventually, start a project and launch the first question.
    
    * Rename auto start app variables
    
    * Finish project auto start, auto start question, redirect actions
    
    Complete work on automatically starting a project defined in
    by a System Setting.
    
    Allow the auto start system setting to define the project template
    (e.g. compliance app) to use and even the module to automatically
    start.
    
    Also add new question actions to redirect to project page
    or project component after answering a question.
    
    TO DO:
    - Add error handling (e.g., missing/incorrect project)
    - Add tests
    
    * Bump VERSION
    
    Co-authored-by: Greg Elin <Greg Elin>
    Co-authored-by: Greg Elin <greg.elin@govready.com>

--- system_settings/models.py
+++ system_settings/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from jsonfield import JSONField
 
 # This is just a bare minimum for demo. Needs to be elaborated upon. Classified intentionally left out because it has complicated implications.
 
@@ -24,13 +25,12 @@ class Sitename(models.Model):
     def __str__(self):
         return f'{self.sitename}'
 
-
 class SystemSettings(models.Model):
   """Model for various system settings for install of GovReady"""
 
-  setting = models.CharField(max_length=200, unique=True)
+  setting = models.CharField(max_length=200, unique=True, help_text="Name of System Setting")
   active = models.BooleanField(default=False)
-
+  details = models.JSONField(blank=True, null=True, help_text="Value objects of System Setting in JSON")
 
   def __str__(self):
     return self.setting
@@ -50,3 +50,7 @@ class SystemSettings(models.Model):
   @classmethod
   def enable_experimental_evidence(cls):
     return cls.objects.get(setting="enable_experimental_evidence").active
+
+  @classmethod
+  def enable_auto_start_project(cls):
+    return cls.objects.get(setting="auto_start_project").active
