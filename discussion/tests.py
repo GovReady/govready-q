9fb05d37da3eab2d9688921f1544835deba11ea3
Greg Elin <greg.elin@govready.com>
2022-01-04 05:07:13 -0600

    Fix test in discussion

--- discussion/tests.py
+++ discussion/tests.py
@@ -83,6 +83,8 @@ class DiscussionTests(SeleniumTest):
         self.fill_field("#id_login", self.user.username)
         self.fill_field("#id_password", self.user_pw)
         self.click_element("form#login_form button[type=submit]")
+        if "Warning Message" in self.browser.title:
+            self.click_element("#btn-accept")
         self.assertRegex(self.browser.title, "Your Compliance Projects")
 
     # def _new_project(self):
@@ -96,7 +98,6 @@ class DiscussionTests(SeleniumTest):
     #     # wait_for_sleep_after(lambda: self.click_element("#start-project"))
     #     wait_for_sleep_after(lambda: self.assertRegex(self.browser.title, "I want to answer some questions on Q."))
 
-
     def _new_project(self):
         self.browser.get(self.url("/projects"))
 
@@ -109,9 +110,6 @@ class DiscussionTests(SeleniumTest):
 
         m = re.match(r"http://.*?/projects/(\d+)/", self.browser.current_url)
         self.current_project = Project.objects.get(id=m.group(1))
-
-
-
 
     def _start_task(self):
         # Assumes _new_project() just finished.
