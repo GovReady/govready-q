063aad3d97b9d5af4cce4fd9d83938a83165c055
Greg Elin <gregelin@govready.com>
2022-05-16 16:35:42 -0400

    Remove redundancy in control migrations 0072

--- controls/migrations/0072_auto_20220515_1548.py
+++ controls/migrations/0072_auto_20220515_1548.py
@@ -10,36 +10,9 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterModelOptions(
-            name='historicaldeployment',
-            options={'get_latest_by': ('history_date', 'history_id'), 'ordering': ('-history_date', '-history_id'), 'verbose_name': 'historical deployment', 'verbose_name_plural': 'historical deployments'},
-        ),
-        migrations.AlterModelOptions(
-            name='historicalstatement',
-            options={'get_latest_by': ('history_date', 'history_id'), 'ordering': ('-history_date', '-history_id'), 'verbose_name': 'historical statement', 'verbose_name_plural': 'historical statements'},
-        ),
-        migrations.AlterModelOptions(
-            name='historicalsystemassessmentresult',
-            options={'get_latest_by': ('history_date', 'history_id'), 'ordering': ('-history_date', '-history_id'), 'verbose_name': 'historical system assessment result', 'verbose_name_plural': 'historical system assessment results'},
-        ),
         migrations.AddField(
             model_name='system',
             name='info',
             field=models.JSONField(blank=True, default=dict, help_text='JSON object representing additional system information'),
-        ),
-        migrations.AlterField(
-            model_name='historicaldeployment',
-            name='history_date',
-            field=models.DateTimeField(db_index=True),
-        ),
-        migrations.AlterField(
-            model_name='historicalstatement',
-            name='history_date',
-            field=models.DateTimeField(db_index=True),
-        ),
-        migrations.AlterField(
-            model_name='historicalsystemassessmentresult',
-            name='history_date',
-            field=models.DateTimeField(db_index=True),
         ),
     ]
