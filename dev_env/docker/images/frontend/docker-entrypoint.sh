c97db2a518bee32a99c1f7d9dd1efd7e3fe4c544
Sergio Falcon <SergioJFalcon@gmail.com>
2022-06-10 14:14:09 -0500

    updated npm

--- dev_env/docker/images/frontend/docker-entrypoint.sh
+++ dev_env/docker/images/frontend/docker-entrypoint.sh
@@ -12,6 +12,7 @@ mkdir -p static
 
 # Installs based on the package-lock.json.
 #npm --max-old-space-size=2048 ci --prefer-offline --no-audit --progress=false
+npm install -g npm@8.12.1
 
 npm install --prefer-offline --no-audit --progress=false
 
