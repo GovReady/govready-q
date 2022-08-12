1753031ab7af89e1520e8da8578b6139590aed16
Greg Elin <gregelin@govready.com>
2022-08-11 21:26:10 -0500

    [WIP] Add workflowinstancesets and bulk create/delete workflowinstance methods

--- workflow/admin.py
+++ workflow/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import WorkflowImage, WorkflowInstance
+from .models import WorkflowImage, WorkflowInstanceSet, WorkflowInstance
 from simple_history.admin import SimpleHistoryAdmin
 from django_json_widget.widgets import JSONEditorWidget
 from jsonfield import JSONField
@@ -10,18 +10,22 @@ from guardian.admin import GuardedModelAdmin
 # Register your models here.
 class WorkflowImageAdmin(GuardedModelAdmin):
     list_display = ('id', 'name', 'uuid')
-    # actions = ["export_as_csv"]
     formfield_overrides = {
         models.JSONField: {'widget': JSONEditorWidget(attrs={'style': 'height: 40em; width: 650px; margin-left: 160px;'})},
     }
+
+
+class WorkflowInstanceSetAdmin(GuardedModelAdmin):
+    list_display = ('id', 'name', 'uuid', 'description')
 
 
 class WorkflowInstanceAdmin(GuardedModelAdmin):
-    list_display = ('id', 'name', 'uuid', 'system')
-    # actions = ["export_as_csv"]
+    list_display = ('id', 'name', 'uuid', 'system', 'workflowinstanceset')
     formfield_overrides = {
         models.JSONField: {'widget': JSONEditorWidget(attrs={'style': 'height: 40em; width: 650px; margin-left: 160px;'})},
     }
 
+
 admin.site.register(WorkflowImage, WorkflowImageAdmin)
+admin.site.register(WorkflowInstanceSet, WorkflowInstanceSetAdmin)
 admin.site.register(WorkflowInstance, WorkflowInstanceAdmin)
--- workflow/models.py
+++ workflow/models.py
@@ -38,6 +38,79 @@ class WorkflowImage(auto_prefetch.Model, TagModelMixin, BaseModel):
         # For debugging.
         return f'<WorkflowImage name="{self.name}" id={self.id}>'
 
+    def create_workflowinstance_obj(self):
+        """Returns a generic workflowinstance unsaved object from workflowimage"""
+        wfinstance = WorkflowInstance()
+        wfinstance.workflowimage = self
+        wfinstance.name = self.name
+        wfinstance.workflow = self.workflow
+        wfinstance.rules = self.rules
+        # add tag
+        return wfinstance
+
+    @transaction.atomic
+    def create_system_worflowinstances(self, filter, system_id_list=[], name=None, description=None):
+        """Create workflowinstances associated with systems as per filter
+            @filter (str) - ALL | INCLUDE | EXCLUDE
+            @system_id_list (list) - list of system ids for filter
+        """
+
+        # create a WorkflowInstanceSet
+        if name is None:
+            name = self.name + " set"
+        if description is None:
+            description = f'Set created from {self.name}'
+        new_wfinstanceset = WorkflowInstanceSet.objects.create(name=name, description=description)
+        print(f'[DEBUG] created new wfinstanceset name={new_wfinstanceset.name}')
+
+        if filter == "ALL":
+            systems = System.objects.all()
+        elif filter == "INCLUDE":
+            systems = System.objects.filter(id in system_id_list)
+        elif filter == "EXCLUDE":
+            systems = System.objects.filter(id not in system_id_list)
+        else:
+            # we have an error
+            raise ValueError (f'Unrecongized filter for system in create_system_workflow_instance: {filter}')
+
+        wfinstances = []
+        for system in systems:
+            wfinstance = self.create_workflowinstance_obj()
+            wfinstance.workflowinstanceset = new_wfinstanceset
+            wfinstance.system = system
+            # tweak instance workflow json
+            # set current item
+            # add tag(s)
+            # update log to indicate created from workflowimage <uuid>
+            wfinstances.append(wfinstance)
+
+        # bulk create wfinstances    
+        new_wfinstances = WorkflowInstance.objects.bulk_create(wfinstances)
+        print(f'[DEBUG] Created {len(new_wfinstances)} instances')
+        return new_wfinstanceset
+
+
+class WorkflowInstanceSet(auto_prefetch.Model, TagModelMixin, BaseModel):
+    name = models.CharField(max_length=100, help_text="Descriptive name", unique=False, blank=True, null=True)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=True, help_text="Unique identifier")
+    workflowimage = auto_prefetch.ForeignKey(WorkflowImage, null=True, related_name="workflowinstancesets", on_delete=models.SET_NULL,
+                                            help_text="WorkflowImage")
+    description = models.CharField(max_length=250, help_text="Brief description", unique=False, blank=True, null=True)
+
+    def __str__(self):
+        return f'<WorkflowInstanceSet name="{self.name}" id={self.id}>'
+
+    def __repr__(self):
+        # For debugging.
+        return f'<WorkflowInstanceSet name="{self.name}" id={self.id}>'
+
+    @transaction.atomic
+    def delete_workflowinstances(self):
+        """Delete all workflowinstances associated with a workflowinstanceset"""
+
+        WorkflowInstance.objects.filter(workflowinstanceset=self).delete()
+        return None
+
 
 class WorkflowInstance(auto_prefetch.Model, TagModelMixin, BaseModel):
     name = models.CharField(max_length=100, help_text="Descriptive name", unique=False, blank=True, null=True)
@@ -45,10 +118,12 @@ class WorkflowInstance(auto_prefetch.Model, TagModelMixin, BaseModel):
     workflow = models.JSONField(blank=True, default=dict, help_text="Workflow object")
     rules = models.JSONField(blank=True, default=dict, help_text="Rules object")
     log = models.JSONField(blank=True, default=dict, help_text="Log object")
-    workflowimage = auto_prefetch.ForeignKey(WorkflowImage, null=True, related_name="workflowimage", on_delete=models.SET_NULL,
+    workflowimage = auto_prefetch.ForeignKey(WorkflowImage, null=True, related_name="workflowinstances", on_delete=models.SET_NULL,
                                             help_text="WorkflowImage")
     # parent = models.ForeignKey(WorkflowImage, related_name="children", on_delete=models.SET_NULL,
     #                                          help_text="Parent WorkflowInstance")
+    workflowinstanceset = auto_prefetch.ForeignKey(WorkflowInstanceSet, related_name='workflowinstances', on_delete=models.SET_NULL, blank=True,
+                                      null=True, help_text="System")
     system = auto_prefetch.ForeignKey(System, related_name='workflowinstances', on_delete=models.CASCADE, blank=True,
                                       null=True, help_text="System")
 
