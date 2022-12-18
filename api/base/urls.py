ae401659bca84b98e1a1cc68d3a045adf28c3b30
Greg Elin <gregelin@govready.com>
2022-12-18 09:21:29 -0500

    Ge/fix swagger scheme (#1792)
    * Dynamically set scheme for swagger

--- api/base/urls.py
+++ api/base/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from siteapp.settings import ACCOUNT_DEFAULT_HTTP_PROTOCOL, GOVREADY_URL
 
 
 def get_swagger_urls():
@@ -11,6 +12,7 @@ def get_swagger_urls():
             default_version='v1',
             description="GovReady API",
         ),
+        url=f"{ACCOUNT_DEFAULT_HTTP_PROTOCOL}://{GOVREADY_URL.hostname}:{GOVREADY_URL.port}/api/v2/",
         public=True,
         permission_classes=(permissions.AllowAny,),
     )
